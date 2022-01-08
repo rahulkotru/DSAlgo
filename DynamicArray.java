6097acae218980e32e7c0f0d451c08f0d306c2b6
Rahul <rahulkotr1@gmail.com>
2022-01-08 15:28:03 +0530

    Compile Dynamic Array Object

--- DynamicArray.java
+++ DynamicArray.java
@@ -32,4 +32,51 @@ public class DynamicArray <T> implements Iterable<T> {
         }
         arr[len++]=elem;
     }
+    public T removeAt(int rm_index){
+        if(rm_index>= len&& rm_index <0) throw new IndexOutOfBoundsException();
+        T data= arr[rm_index];
+        T[] new_arr= (T[]) new Object[len-1];
+        for( int i=0, j=0; i<len; i++,j++)
+        if(i==rm_index) j--;
+        else new_arr[j]=arr[i];
+        arr=new_arr;
+        capacity=--len;
+        return data;
+    }
+    public boolean remove(Object obj){
+        for (int i=0;i<len;i++){
+            if(arr[i].equals(obj)){
+                removeAt(i); return true;
+            }
+        }
+        return false;
+    }
+    public int indexOf(Object obj){
+        for (int i=0;i< len; i++)
+        if (arr[i].equals(obj))
+            return i;
+        return -1;
+    }
+    public boolean contains(Object obj){
+        return indexOf(obj) !=-1;
+    }
+    @Override public java.util.Iterator <T> iterator (){
+        return new java.util.Iterator <T> (){
+            int index =0;
+            public boolean hasNext(){
+                return index < len;}
+                public T next(){
+                    return arr[index++];}
+        };
+    }
+    @Overide public String toString(){
+        if (len==0) return "[]";
+        else{
+            StringBuilder sb= new StringBuilder(len).append("[");
+            for(int i=0; i<len-1;i++)
+            sb.append(arr[i]+",");
+            return sb.append(arr[len-1]+"]").toString();
+        }
+    }
+
 }
