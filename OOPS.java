0349c54f6800a877294a7b682aeb703e6ccac98a
Rahul <rahulkotr1@gmail.com>
2022-01-10 21:30:54 +0530

    Add constructor to Pen class

--- OOPS.java
+++ OOPS.java
@@ -3,8 +3,21 @@
     String type;
 
 
+    Pen(String color, String type){//3 types of constructors; Non-paramaterized, parameterized 
+        //Non para doesnt take or return parameters
+        //Parameterized takes parameters and initializes them to the class
+        /*  
+            p1.color="Blue";  } This part is done inside the constructor itself
+            p1.type="Gel";    }
+            */ 
+        this.color = color;// It is important to assign these objects in the constructor
+        this.type = type;
+        System.out.printf("This is constructor for Pen, it is called only when the class is created\n");
+
+    }
+
     public void write(){
-        System.out.printf("Writing something\n");
+        System.out.println("This pen is a ");//+ this.type+"\n");
     }
 
     public void print_color(){//By convention, all method objects start with lowercase letters; write(),print_color
@@ -22,15 +35,11 @@
 public class OOPS{
     
     public static void main(String[] args){
-        Pen p1=new Pen();
-        p1.color="Blue";
-        p1.type="Gel";
+        Pen p1=new Pen("Blue","Gel"); 
         p1.write();
         p1.print_color();
 
-        Pen p2= new Pen();
-        p2.color="Black";
-        p2.type="Ballpoint";
+        Pen p2= new Pen("Black","Ballpoint");
         p2.print_color();
 
         Student s1= new Student();
