be9a903a82ad5c498c7c2f20453f9819a2e6405e
Rahul <rahulkotr1@gmail.com>
2022-01-14 19:16:25 +0530

    Add even counter in array

--- ArraysUtil.java
+++ ArraysUtil.java
@@ -1,5 +1,5 @@
 public class ArraysUtil{
-    public void printArray(int [] arr){
+    public static void printArray(int [] arr){
         int n=arr.length;
         for (int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
@@ -16,10 +16,33 @@ public class ArraysUtil{
         newArr[3]=2;
         newArr[4]=10;
         printArray(newArr);
+        System.out.println(newArr.length-1);
     }
+    public static int[] evenInt(int [] arr){
+            int count=0;
+            for (int i=0;i<arr.length;i++){
+                if(arr[i]%2==0){
+                    count++;
+                }
+            }
+            int j=0;
+            int[] jarr= new int[count];
+            for (int i=0 ;i<arr.length;i++){
+                if(arr[i]%2==0){
+                    jarr[j]=arr[i];
+                    j++;
+                }
+            }
+            return jarr;
+    }
+
     public static void main(String[] args) {
-        ArraysUtil arr=new ArraysUtil();
-        arr.arrayDemo();
+       // ArraysUtil arr=new ArraysUtil();
+       // arr.arrayDemo();
+        int newInt[]={5,4,6,3,1};
+        printArray(newInt);
+        int res[]= evenInt(newInt);
+        printArray(res);
 
     }
 }
