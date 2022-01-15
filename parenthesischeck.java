ec42374ce44611ad646445d09abc738a69471f47
Rahul <rahulkotr1@gmail.com>
2022-01-15 23:37:01 +0530

    Add parenthesischecker class

---
+++ parenthesischeck.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+public class parenthesischeck{
+    public Boolean checkparity(String str){
+        Stack <Character> stack = new Stack<>();
+        for( char c : str.toCharArray()){
+            if(c=='('|| c=='{'|| c=='['){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else{
+                    char.top = stack.peek();
+                    if((c ==')'&& top=='('|| c=='}'&& top=='{'|| c==']'&& c=='[')){
+                        stack.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str='{}';
+        System.out.println(checkparity(str));
+    }
+    
+}
