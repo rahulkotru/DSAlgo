904eeb4fab762f6502917563baaf242f1f29d0a3
Rahul <rahulkotr1@gmail.com>
2022-01-15 22:00:54 +0530

    Fixed Stacks

--- Stacks.java
+++ Stacks.java
@@ -2,14 +2,14 @@ public class Stacks{
     private ListNode top;//Instance Variables
     private int length;
 
-    private static class ListNode{
+    private class ListNode{
         private int data;
         private ListNode next;
-    }
+    
 
-    public ListNode(int data){//Constructor for LinkNode
-        this.data=data;
-        this.next=null;
+        public ListNode(int data){//Constructor for LinkNode
+            this.data=data;
+        }
     }
     public Stacks(){//Constructor
         top=null;
@@ -20,18 +20,18 @@ public class Stacks{
     }
     public void push(int data){
         ListNode temp= new ListNode(data);
-        temp.next=next;
+        temp.next=top;
         top=temp;
         length++;
     }
-    public void pop(){
+    public int pop(){
         int result =top.data;
         length--;
         return result;
     }
     
     public static void main(String[] args) {
-        Stack newStack= new Stack();
+        Stacks newStack= new Stacks();
         newStack.push(10);
         newStack.push(20);
         newStack.push(44);
