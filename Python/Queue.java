e6b372dfea7cb443ef505a230cfd291a3b875196
Rahul <rahulkotr1@gmail.com>
2022-01-17 20:56:24 +0530

    Add queue class

---
+++ Python/Queue.java
@@ -0,0 +1,15 @@
+public class Queue{
+    private ListNode head;
+    private ListNode tail;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+
+        }
+    }
+}
