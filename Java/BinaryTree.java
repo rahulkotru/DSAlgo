eb4632013d7b4cbc33ad5751eca09c7008a9ae61
Rahul <rahulkotr1@gmail.com>
2022-01-18 21:26:12 +0530

    Add Binary Tree PreOrder Traversal

---
+++ Java/BinaryTree.java
@@ -0,0 +1,24 @@
+public class BinaryTree{
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data=data;
+        }
+    }
+    public void preOrder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
