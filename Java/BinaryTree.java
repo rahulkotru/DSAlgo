d7b06c82454573e91b260d9981f48434b5e00ca1
Rahul <rahulkotr1@gmail.com>
2022-01-18 21:33:42 +0530

    Preorder traversal implemented

--- Java/BinaryTree.java
+++ Java/BinaryTree.java
@@ -10,6 +10,16 @@ public class BinaryTree{
             this.data=data;
         }
     }
+    public void createTree(){
+        TreeNode first= new TreeNode(1);
+        TreeNode second=new TreeNode(2);
+        TreeNode third= new TreeNode(3);
+        TreeNode fourth=new TreeNode(4);
+        root=first;
+        first.left=second;
+        first.right=third;
+        second.left=fourth;
+    }
     public void preOrder(TreeNode root){
         if(root==null){
             return;
@@ -19,6 +29,8 @@ public class BinaryTree{
         preOrder(root.right);
     }
     public static void main(String[] args) {
-        
+        BinaryTree tr=new BinaryTree();
+        tr.createTree();
+        tr.preOrder(tr.root);
     }
 }
