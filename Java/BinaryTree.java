a433de8b0cda1a90cfd0820ffd8ffca36884bd6a
Rahul <rahulkotr1@gmail.com>
2022-01-19 23:11:07 +0530

    Add iterativePostOrder Traversal

--- Java/BinaryTree.java
+++ Java/BinaryTree.java
@@ -11,6 +11,7 @@ public class BinaryTree{
             this.data=data;
         }
     }
+
     public void createTree(){
         TreeNode first= new TreeNode(1);
         TreeNode second=new TreeNode(2);
@@ -74,14 +75,31 @@ public class BinaryTree{
         }
     }
     public void iterativePostOrder(TreeNode root){
-        if(root==null){
-            return;
-        }
+        TreeNode current=root;
         Stack<TreeNode> stack=new Stack<>();
-        TreeNode temp=root;
-        System.out.println("");
-        
+        while(current!=null||!stack.isEmpty()){
+            if(current!=null){
+                stack.push(current);
+                current=current.left;
+            }
+            else{
+                TreeNode temp=stack.peek().right;
+                if(temp==null){
+                    temp=stack.pop();
+                    System.out.println(temp.data+" ");
+                    while(!stack.isEmpty()&&temp==stack.peek().right){
+                        temp=stack.pop();
+                        System.out.println(temp.data+"");
+                    }
+                }
+                else{
+                current=temp;
+                }
+            }
+
+        }
     }
+
     public void recursivePostOrder(TreeNode root){
         if(root==null){
             return;
@@ -98,5 +116,6 @@ public class BinaryTree{
         tr.recursiveInOrder(tr.root);
         tr.iterativeInOrder(tr.root);
         tr.recursivePostOrder(tr.root);
+        tr.iterativePostOrder(tr.root);
     }
 }
