ad3cdeb04a22934730c720a97a5c028ac8fef395
Rahul <rahulkotr1@gmail.com>
2022-01-20 13:52:53 +0530

    Dynamic Array

--- Java/DynamicArray.java
+++ Java/DynamicArray.java
@@ -5,17 +5,27 @@ public class DynamicArray <T> implements Iterable<T> {
     private int len = 0;
     private int capacity = 0;
     
-    public DynamicArray() {this(16);}
+    public DynamicArray() {
+        this(16);
+    }
     public DynamicArray(int capacity) {
         if (capacity < 0) throw new IllegalArgumentException("Illegal Capacity: " + capacity);
         this.capacity = capacity;
         arr=(T[]) new Object[capacity];
     }
-    public int size() {return len;}//size Method
-    public boolean isEmpty() {return size()==0;}//isEmpty Method
+    public int size() {
+        return len;
+    }//size Method
+    public boolean isEmpty() {
+        return size()==0;
+    }//isEmpty Method
 
-    public T get(int index){return array[index];}//get Method
-    public void set(int index, T elem) {arr[index]=elem;}//set Method
+    public T get(int index){
+        return array[index];
+    }//get Method
+    public void set(int index, T elem) {
+        arr[index]=elem;
+    }//set Method
     public void clear(){//Clear Method
         for (int i=0;i<capacity;i++)
         arr[i]=null;
