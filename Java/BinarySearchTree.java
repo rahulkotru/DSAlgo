b20e5aa0661dd30f7fa8b5d1334ad2a0f74c7043
Rahul <rahulkotr1@gmail.com>
2022-01-20 22:31:03 +0530

    Add BinarySearchTree

---
+++ Java/BinarySearchTree.java
@@ -0,0 +1,16 @@
+public class BinarySearchTree{
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data=data;
+            }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
