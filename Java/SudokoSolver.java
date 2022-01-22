13c8febaa6d45bf5fba58f730a68b6d9b200702d
Rahul <rahulkotr1@gmail.com>
2022-01-22 23:17:56 +0530

    Add check methods

--- Java/SudokoSolver.java
+++ Java/SudokoSolver.java
@@ -3,6 +3,13 @@ public SudokoSolver{
         
     }
     private static boolean isNumber(int [][],int number, int row){
-        
+        for(int i=0;i<GRID_SIZE;i++){
+
+        }
+    }
+    private static boolean isNumber(int [][],int number, int coloumn){
+        for(int i=0;i<GRID_SIZE;i++){
+            
+        }
     }
 }
