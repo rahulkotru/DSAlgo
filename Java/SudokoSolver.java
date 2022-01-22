869819a7614b6004739a5c86ff17ca968dbf119f
Rahul <rahulkotr1@gmail.com>
2022-01-22 23:22:26 +0530

    define grid dimensions

--- Java/SudokoSolver.java
+++ Java/SudokoSolver.java
@@ -1,15 +1,21 @@
 public SudokoSolver{
     public static void main(String[] args) {
+        private static final int GRID_SIZE=9;
         
+        int [][] board={};
     }
-    private static boolean isNumber(int [][],int number, int row){
+    private static boolean isNumber(int [][] board,int number, int row){
+        for(int i=0;i<GRID_SIZE;i++){
+            if(board)
+        }
+    }
+    private static boolean isNumberInBox(int [][] board,int number, int coloumn){
         for(int i=0;i<GRID_SIZE;i++){
 
         }
     }
-    private static boolean isNumber(int [][],int number, int coloumn){
-        for(int i=0;i<GRID_SIZE;i++){
-            
-        }
+        private static boolean isNumberColoumn(int [][] board,int number, int coloumn){
+        
+        
     }
 }
