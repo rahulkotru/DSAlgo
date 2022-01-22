94c959ea9a5c5008ea917eb3183fc423c2d7582c
Rahul <rahulkotr1@gmail.com>
2022-01-22 23:26:18 +0530

    define methods in box and in row

--- Java/SudokoSolver.java
+++ Java/SudokoSolver.java
@@ -6,16 +6,22 @@ public SudokoSolver{
     }
     private static boolean isNumber(int [][] board,int number, int row){
         for(int i=0;i<GRID_SIZE;i++){
-            if(board)
+            if(board[row][i]==number){
+                return true;
+            }
         }
+        return false;
     }
     private static boolean isNumberInBox(int [][] board,int number, int coloumn){
         for(int i=0;i<GRID_SIZE;i++){
-
+            if([i][coloumn]==number){
+                return true;
+            }
         }
+        return false;
     }
         private static boolean isNumberColoumn(int [][] board,int number, int coloumn){
-        
+            int localBoxRow
         
     }
 }
