99b9c27f056d29e96fdb58233e87235cb9df17e6
Rahul <rahulkotr1@gmail.com>
2022-01-23 16:14:50 +0530

    Defined Practice matrix

--- Java/SudokoSolver.java
+++ Java/SudokoSolver.java
@@ -2,7 +2,16 @@ public SudokoSolver{
     public static void main(String[] args) {
         private static final int GRID_SIZE=9;
         
-        int [][] board={};
+        int [][] board={{7,0,2,0,5,0,6,0,0},
+                        {0,0,0,0,0,3,0,0,0},
+                        {1,0,0,0,0,9,5,0,0},
+                        {8,0,0,0,0,0,0,9,0},
+                        {0,4,3,0,0,0,7,5,0},
+                        {0,9,0,0,0,0,0,0,8},
+                        {0,0,9,7,0,0,0,0,5},
+                        {0,0,0,2,0,0,0,0,0},
+                        {0,0,7,0,4,0,2,0,3}
+                    };
     }
     private static boolean isNumber(int [][] board,int number, int row){
         for(int i=0;i<GRID_SIZE;i++){
