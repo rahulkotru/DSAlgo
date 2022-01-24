126f72c2c29b6bd2e4b0bc3a2eea24d354ff202e
Rahul <rahulkotr1@gmail.com>
2022-01-24 22:30:01 +0530

    Define search matrix

--- Java/matrixSearch.java
+++ Java/matrixSearch.java
@@ -17,6 +17,12 @@ public class matrixSearch{
         System.out.println("Not found");
     }
     public static void main(String[] args){
-
+        int [][] matrix={
+            {10,20,30,40},
+            {15,25,35,45},
+            {27,29,37,48},
+            {32,33,39,51}
+        };
+        
     }
 }
