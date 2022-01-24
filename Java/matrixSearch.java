7a15560f84f64b4616fba58c34e8a1bff661a1e8
Rahul <rahulkotr1@gmail.com>
2022-01-24 22:14:05 +0530

    compiled matrixSearch method

--- Java/matrixSearch.java
+++ Java/matrixSearch.java
@@ -1,6 +1,20 @@
 public class matrixSearch{
-    public void search(int [][] matrix){
-
+    public void search(int [][] matrix, int n, int x){
+        int i=0;
+        int j=n-1;
+        while(i<n&& j>=0){
+            if(matrix[i][j]==x){
+                System.out.println("x found at -"+i+","+j);
+                return;
+            }
+            if(matrix[i][j]>x){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        System.out.println("Not found");
     }
     public static void main(String[] args){
 
