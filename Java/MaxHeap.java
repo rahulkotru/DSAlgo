a75e13b812c16402aa794085f2bc64a8c950ce5d
Rahul <rahulkotr1@gmail.com>
2022-01-25 23:18:30 +0530

    Add maxHeap resize, insert and swim methods

--- Java/MaxHeap.java
+++ Java/MaxHeap.java
@@ -11,6 +11,30 @@ public class MaxHeap{
     public int size(){
         return n;
     }
+    public void resize(int capacity){
+        Integer [] temp= new Integer[capacity];
+        for(int i=0;i<heap.length;i++){
+            temp[i]=heap[i];
+        }
+        heap=temp;
+
+    }
+    public void insert(int x){
+        if(n==heap.length-1){
+            resize(2*heap.length);
+        }
+        n++;
+        heap[n]=x;
+        swim(n);
+    }
+    private void swim(int k){
+        while(k>1&&heap[k/2]<heap[k]){
+            int temp=heap[k];
+            heap[k]=heap[k/2];
+            heap[k/2]=temp;
+            k=k/2;
+        }
+    }
     public static void main(String[] args) {
         MaxHeap mp = new MaxHeap(3);
         System.out.println(mp.size());
