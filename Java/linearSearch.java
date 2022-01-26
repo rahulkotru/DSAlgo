8d64aa6fef9c6328434754925636c38a081f0fda
Rahul <rahulkotr1@gmail.com>
2022-01-26 22:59:22 +0530

    Add LinearSearch method array

--- Java/linearSearch.java
+++ Java/linearSearch.java
@@ -1,5 +1,16 @@
 public class linearSearch{
+    public static void linSearch(int[] arr,int n){
+        for (int i =0;i<arr.length;i++){
+            if(arr[i]==n){
+                System.out.println("Element found at position"+n);
+            }
+            else{
+                System.out.println("Element not found");
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[]={5,1,9,2,10,15,20};
+        linSearch(arr,2);
     }
 }
