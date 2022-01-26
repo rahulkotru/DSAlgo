9bf95e5ee0b43e005fdec1ec10d7240779b88f7e
Rahul <rahulkotr1@gmail.com>
2022-01-26 23:31:34 +0530

    leetcode problem 35

--- Java/linearSearch.java
+++ Java/linearSearch.java
@@ -28,6 +28,23 @@ public class linearSearch{
                 start=mid+1;            }
         }
     }
+    public static int searchInsertPosition(int num[],int num){
+        int start=0;
+        int end=num.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==num) {
+                return mid;
+            }
+            if(num<arr[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
     public static void main(String[] args) {
         int arr[]={5,6,7,3,2,8,1,0};
         linSearch(arr,2);
