92e8f880e9752c85a92eae4f1a104a114a0dee49
Rahul <rahulkotr1@gmail.com>
2022-01-27 23:30:36 +0530

    Complete mergeArr method

--- Java/mergeArrays.java
+++ Java/mergeArrays.java
@@ -2,7 +2,7 @@ public class mergeArrays{
     static int [] mergeArr(int [] a1,int [] a2){
         int [] merger= new int[a1.length + a2.length];
         int i=0; int j=0; int k=0;
-        while(i<n&&j<m){
+        while(i<a1.length&&j<a2.length){
             if(a1[i]<a2[j]){
                 merger[k]=a1[i];
                 i++;
@@ -13,7 +13,15 @@ public class mergeArrays{
             }
             k++;
         }
-        while
+        while(i<a1.length){
+            merger[k]=a1[i];
+            i++;k++;
+        }
+        while(j<a2.length){
+            merger[k]=a2[j];
+            k++;j++;
+        }
+        return merger;
     }
     public static void main(String[] args) {
         
