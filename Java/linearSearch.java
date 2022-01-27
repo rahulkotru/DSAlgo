a7b30107fd49fb753fe89cef47a2caa519747041
Rahul <rahulkotr1@gmail.com>
2022-01-27 20:12:07 +0530

    Implement insertion sort method

--- Java/linearSearch.java
+++ Java/linearSearch.java
@@ -70,8 +70,9 @@ public class linearSearch{
             int j=i-1;
             while(j>=0&&arr[j]>temp){
                 arr[j+1]=arr[j];
-                
+                j=j-1;
             }
+            arr[j+1]=temp;
         }
     }
     public static void main(String[] args) {
