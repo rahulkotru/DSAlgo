446eff84952e7ae5733d251a0837fda55f7bbdce
Rahul <rahulkotr1@gmail.com>
2022-01-27 23:25:18 +0530

    Add mergeArray class

---
+++ Java/mergeArrays.java
@@ -0,0 +1,10 @@
+public class mergeArrays{
+    static int [] mergeArr(int [] a1,int [] a2){
+        int [] merger= new int[a1.length + a2.length];
+        int i=0; int j=0; int k=0;
+        while(i<)
+    }
+    public static void main(String[] args) {
+        
+    }
+}
