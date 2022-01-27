6a33dea20f78172da3d95cfb032e571a50dffff3
Rahul <rahulkotr1@gmail.com>
2022-01-27 20:27:01 +0530

    Add selection sort method

--- Java/linearSearch.java
+++ Java/linearSearch.java
@@ -75,6 +75,20 @@ public class linearSearch{
             arr[j+1]=temp;
         }
     }
+    static void selectionSort(int arr[]){
+        int n=arr.length;
+        for =(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            
+        }
+    }
     public static void main(String[] args) {
         int arr[]={5,6,7,3,2,8,1,0};
         linSearch(arr,2);
