e93ee6b634ec3c76a55f1a10448b990053db86b7
Rahul <rahulkotr1@gmail.com>
2022-01-28 01:00:27 +0530

    LeetCode challenge for finding median of 2 sorted arrays

--- Java/mergeArrays.java
+++ Java/mergeArrays.java
@@ -23,13 +23,50 @@ public class mergeArrays{
         }
         return merger;
     }
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int med=0;
+        int reg[]=new int[nums1.length+nums2.length];
+        int i=0;int j=0; int k=0;
+        while(i<nums1.length && j<nums2.length){
+            reg[k]=(nums1[i]>nums2[j])? nums2[j]:nums1[i];
+            if(nums1[i]>nums2[j]){
+                j++;
+                
+            }
+            else{
+                i++;
+                
+            }
+            k++;
+        }
+            while(i<nums1.length){
+                reg[k]=nums1[i];
+                i++;k++;
+            }
+            while(j<nums2.length){
+                reg[k]=nums2[j];
+                j++;k++;
+            }
+        med=reg.length%2;
+        if(med!=0){
+            double me=reg[(int) ((reg.length/2))];
+            return me;
+        }
+        else{
+            double me=(reg[reg.length/2]+reg[(reg.length/2)-1])/2.0;
+               return me;         
+        }
+        
+    }
     public static void main(String[] args) {
-        int[] arr={3,5,7,8};
-        int[] narr={1,2,4,6};
+        int[] arr={3,5,7};
+        int[] narr={1,2,4};
         int [] z=mergeArr(arr,narr);
-        for(int i=0;i<z.length-1;i++){
+        double x=findMedianSortedArrays(arr,narr);
+        for(int i=0;i<z.length;i++){
             System.out.print("-->"+z[i]);
         }
-
+        System.out.print("-->"+x);
     }
+
 }
