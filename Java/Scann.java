cb7595ae0a600bb370dcbebd24840d4ec903811f
Rahul <rahulkotr1@gmail.com>
2022-02-01 22:21:08 +0530

    Add scanner class

--- Java/Scann.java
+++ Java/Scann.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class Scann{
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        
+        System.out.println("Enter an integer");
+        int a=scan.nextInt();
+        System.out.println("Enter a string");
+        String b=scan.nextLine();
+        System.out.printf("you ha");
     }
 }
