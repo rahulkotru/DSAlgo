594e2cb977461396773380dab35f0cc080f99c69
Rahul <rahulkotr1@gmail.com>
2022-02-09 20:52:03 +0530

    Stack using collections module

--- Python/StackUsingList.py
+++ Python/StackUsingList.py
@@ -1,13 +1,8 @@
-stack=[]
-stack.append(0)
-stack.append(20)
-stack.append(3)
-stack#check elements in a stack
-stack.pop()#pop elements in stack
+import collections
+stack=collections.deque()
+stack
+stack.append(10)
+stack.append(12)
+stack.append(44)
+stack
 stack.pop()
-stack.pop()
-len(stack)==0#check elements in stack
-not stack
-stack.append(9)
-stack.append(3)
-stack[-1]#check last element of stack
