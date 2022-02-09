115701fb26a6e354cdbbe0b37f16a1e557bbeb39
Rahul <rahulkotr1@gmail.com>
2022-02-09 20:45:07 +0530

    Stack using list in python

--- Python/StackUsingList.py
+++ Python/StackUsingList.py
@@ -2,4 +2,12 @@ stack=[]
 stack.append(0)
 stack.append(20)
 stack.append(3)
-stack
+stack#check elements in a stack
+stack.pop()#pop elements in stack
+stack.pop()
+stack.pop()
+len(stack)==0#check elements in stack
+not stack
+stack.append(9)
+stack.append(3)
+stack[-1]#check last element of stack
