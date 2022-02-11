e0280c40ba81151585359d036c29e32346e35dd9
Rahul <rahulkotr1@gmail.com>
2022-02-11 21:52:35 +0530

    Implement BubbleSort

--- Python/BubbleSort.py
+++ Python/BubbleSort.py
@@ -5,3 +5,13 @@ for k in range(num):
     list1.append(int(input()))
 
 print("Unsorted array: ",list1)
+
+for j in range(len(list1)-1,0,-1):
+    for i in range(j):
+        if list1[i]>list1[i+1]:
+            list1[i],list1[i+1]=list1[i+1],list1[i]
+            print(list1)
+        else:
+            print(list1)
+        print()
+print("Sorted List: ",list1)
