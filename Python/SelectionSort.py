5803ae63916de94a71cb63d1a139843b5d791011
Rahul <rahulkotr1@gmail.com>
2022-02-11 21:23:15 +0530

    iterating over loop for max sort

--- Python/SelectionSort.py
+++ Python/SelectionSort.py
@@ -2,7 +2,7 @@
 list1=[56,3,2,78,6,0]
 
 for i in range(len(list1)):
-    min_val=min(list1[i:])
+    min_val=max(list1[i:])
     min_ind=list1.index(min_val)
     list1[i],list1[min_ind]=list1[min_ind],list1[i]
     print(list1)
