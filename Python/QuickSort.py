036e1e8531012c0ea5f787d304a1affe23ab3425
Rahul <rahulkotr1@gmail.com>
2022-02-11 23:34:00 +0530

    Define quicksorrt

--- Python/QuickSort.py
+++ Python/QuickSort.py
@@ -14,3 +14,8 @@ def pivot_place(list1,first,last):
                 list1[left],list1[right]=list1[right],list1[left]
         list1[first],list1[right]=list1[right],list1[first]
         return right
+
+def quicksort(list1,first,last):
+    p=pivot_place(list1,first,last)
+    quicksort(list1,first,p-1)
+    quicksort(list1,p+1,last)
