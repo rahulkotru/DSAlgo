6a00f8fcde3363c9a0c21b6eb735138db3dab470
Rahul <rahulkotr1@gmail.com>
2022-02-11 21:49:35 +0530

    Add BubbleSort

---
+++ Python/BubbleSort.py
@@ -0,0 +1,7 @@
+# Simple sorting algorithm which compares n elements in list
+list1=[]
+num=int(input("Length of array: "))
+for k in range(num):
+    list1.append(int(input()))
+
+print("Unsorted array: ",list1)
