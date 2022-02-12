d5e704c3e0efd09715ce4ecea3d1d2c52242d0ac
Rahul <rahulkotr1@gmail.com>
2022-02-12 21:14:44 +0530

    Add merge method for merge sort

--- Java/mergeSort.java
+++ Java/mergeSort.java
@@ -1,5 +1,5 @@
 public class mergeSort{
-    public void printArray(int[] arr){
+    static void printArray(int[] arr){
         int n=arr.length;
         for(int i=0;i<n;i++){
             System.out.println(arr[i]+" ");
@@ -9,9 +9,15 @@ public class mergeSort{
     public void sort(int [] arr,int [] temp, int low, int high){
         if(low<high){
             int mid=low+(high-low)/2;
+            sort(arr,temp,low,high);
+            sort(arr,temp,low:mid+1,high);
+            merge(arr,temp,low,mid,high);
         }
     }
+    private void merge(int[] arr,int)
     public static void main(String[] args) {
-        int 
+        int [] arr=new int[]{9,5,2,4,3};
+        mergeSort ms=new mergeSort();
+        printArray(arr);
     }
 }
