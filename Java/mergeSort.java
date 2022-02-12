e34d6381a4188e558cac6e57301571e9ab6c62c9
Rahul <rahulkotr1@gmail.com>
2022-02-12 20:55:31 +0530

    Add sort method for merge sort

---
+++ Java/mergeSort.java
@@ -0,0 +1,17 @@
+public class mergeSort{
+    public void printArray(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public void sort(int [] arr,int [] temp, int low, int high){
+        if(low<high){
+            int mid=low+(high-low)/2;
+        }
+    }
+    public static void main(String[] args) {
+        int 
+    }
+}
