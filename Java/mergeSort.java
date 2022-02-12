008bb05442ac24266f1a28d0d8e45d4cd1247f89
Rahul <rahulkotr1@gmail.com>
2022-02-12 21:25:28 +0530

    Testing

--- Java/mergeSort.java
+++ Java/mergeSort.java
@@ -14,10 +14,34 @@ public class mergeSort{
             merge(arr,temp,low,mid,high);
         }
     }
-    private void merge(int[] arr,int)
+    private static void merge(int[] arr,int[] temp,int low,int high){
+        for (int i=low;i<=high;i++){
+            temp[i]=arr[i];
+        }
+        int i =low;
+        int j=mid+1;
+        int k=low;
+        while(i<=mid&&j<=high){
+            if(temp[i]<=temp[j]){
+                arr[k]=temp[i];
+                i++;
+            }
+            else{
+                arr[k]=temp[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            arr[k]=temp[i];
+            k++;
+            i++;
+        }
+    }
     public static void main(String[] args) {
         int [] arr=new int[]{9,5,2,4,3};
-        mergeSort ms=new mergeSort();
+        printArray(arr);
+        sort(arr,new int[arr.length],low:0,high:arr.length-1);
         printArray(arr);
     }
 }
