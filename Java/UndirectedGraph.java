131a54e684da0056271311bb6cde61bd5ad6f494
Rahul <rahulkotr1@gmail.com>
2022-02-14 22:56:39 +0530

    Print graph

--- Java/UndirectedGraph.java
+++ Java/UndirectedGraph.java
@@ -12,12 +12,26 @@ public class UndirectedGraph{
         this.adjMatrix[v][u]=1;
         E++;
     }
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(V+" Vertices and "+E+" Edges\n");
+        for(int v=0;v<V;v++){
+            sb.append(v+": ");
+            for (int w: adjMatrix[v]){
+                sb.append(w+" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+
+    }
     public static void main(String[] args){
         UndirectedGraph graph=new UndirectedGraph(4);
         graph.addEdge(0,1);
         graph.addEdge(2,3);
         graph.addEdge(3,0);
         graph.addEdge(1,2);
+        System.out.println(graph);
     }
 
 }
