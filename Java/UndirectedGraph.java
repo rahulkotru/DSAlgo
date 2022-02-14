a8c205d03f63e75d9d7faeb1aff06855f9be46d7
Rahul <rahulkotr1@gmail.com>
2022-02-14 22:03:26 +0530

    Initializing new graph

--- Java/UndirectedGraph.java
+++ Java/UndirectedGraph.java
@@ -1,4 +1,15 @@
 public class UndirectedGraph{
-    
+    int [][] adjMatrix;
+    public UndirectedGraph(int nodes){
+        this.adjMatrix=new adjMatrix[nodes][nodes];
+    }
+    public void addEdge(int u,int v){
+        this.adjMatrix[u][v]=1;
+        this.adjMatrix[v][u]=1;
+    }
+    public static void main(String[] args){
+        UndirectedGraph graph=new UndirectedGraph(4);
+
+    }
 
 }
