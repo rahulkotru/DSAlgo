0e1d3e2a13aa8d4dc97d4d2db27cb6d649fbff28
Rahul <rahulkotr1@gmail.com>
2022-02-15 21:39:48 +0530

    Printing Fibonacci

--- Java/Problem/Fibonacci.java
+++ Java/Problem/Fibonacci.java
@@ -2,8 +2,8 @@ public class Fibonacci{
     public static int Fibo(int n){
         if(n==0) return 0;
         if(n==1) return 1;
-        int left=fib(n-1);
-        int right=fib(n-2);
+        int left=Fibo(n-1);
+        int right=Fibo(n-2);
         return left+right;
     }
     public static void main(String[] args) {
