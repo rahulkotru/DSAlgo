1fff3f806db9bdd0a2b71d0115264b632b85bd27
Rahul <rahulkotr1@gmail.com>
2022-02-15 21:38:32 +0530

    Fibonacci

--- Java/Problem/Fibonacci.java
+++ Java/Problem/Fibonacci.java
@@ -1,3 +1,12 @@
 public class Fibonacci{
-    
+    public static int Fibo(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int left=fib(n-1);
+        int right=fib(n-2);
+        return left+right;
+    }
+    public static void main(String[] args) {
+        
+    }
 }
