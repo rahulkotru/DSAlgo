0d568e37f92441a9eee26ad153ea781ceb60fb99
Rahul <rahulkotr1@gmail.com>
2022-02-15 23:16:34 +0530

    Print graph

--- Java/Problem/Fibonacci.java
+++ Java/Problem/Fibonacci.java
@@ -9,15 +9,15 @@ public class Fibonacci{
     }
     public static long fibo(int n){
         long[] arr=new long[n+2];
-        if(n==0) return 0;
-        if (n==1) return 1;
-        for (int i=2;i<n;i++){
+        arr[0] = 0;
+        arr[1]= 1;
+        for (int i=2;i<=n;i++){
             arr[i]=arr[i-1]+arr[i-2];
         }
         return arr[n];
     }
     public static void main(String[] args) {
-        int u=Fibo(8);
+        long u=fibo(8);
         System.out.println(u);
     }
 }
