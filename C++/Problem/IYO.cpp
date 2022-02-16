f2aabe1860bd92e7a6836c3b7ea078184827d98a
Rahul <rahulkotr1@gmail.com>
2022-02-16 20:58:30 +0530

    Addition

---
+++ C++/Problem/IYO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter num1: ";
+    cin>> num1;
+    cout <<"Enter num2";
+    cin>> num2;
+    cout <<"The sum is: "<<num1+num2;
+}
