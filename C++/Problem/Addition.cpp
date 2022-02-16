b8f12d0b84fadb0f53650aba7a50230d57bbbe4c
Rahul <rahulkotr1@gmail.com>
2022-02-16 20:26:37 +0530

    Addition

--- C++/Problem/Addition.cpp
+++ C++/Problem/Addition.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main(){
     int a=9,b=3;
     float pi=3.14;
+    char g='k';
     int c=a+b;
-    cout << c <<"\nThis is value of Pi: " <<pi;
+    cout << c <<"\nThis is value of Pi: " <<pi <<g;
     return 0;
 }
