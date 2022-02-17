224b0b57f6539aeb625ab2ba5df92474bab31d60
Rahul <rahulkotr1@gmail.com>
2022-02-17 15:36:09 +0530

    Public class

--- C++/OOPs/OOP.cpp
+++ C++/OOPs/OOP.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 class Employee{
+public:
     string Name;
     string Company;
-    int age;
+    int Age;
+
+    void giveIntro(){
+        cout<<"Hi! My name is: "<<Name<<", I work at: "<<Company<<" and I am "<<Age<<" years old."<<endl;
+    }
 };
 int main(){
     Employee E1;
+    E1.Name="Phil";
+    E1.Company="Tata";
+    E1.Age=24;
+    E1.giveIntro();
     return 0;
 }
