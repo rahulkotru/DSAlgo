97d52a7ea526954823ac14e81b7c91d9ed98ffb7
Rahul <rahulkotr1@gmail.com>
2022-02-23 22:11:23 +0530

    LeetCode problems

--- C++/OOPs/TwoSum.cpp
+++ C++/OOPs/TwoSum.cpp
@@ -5,8 +5,9 @@ using namespace std;
 class Solution{
 public:
     vector<int> twoSum(vector<int>&nums,int target){
+        vector<int> result;
         for(auto it1=nums.begin(); it!=nums.end();++it){
-            
+            auto it2=find(it+1)
         }
     }
 };
