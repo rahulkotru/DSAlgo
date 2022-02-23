60d444519ee53e3da1ed3db04cd6aa478184286e
Rahul <rahulkotr1@gmail.com>
2022-02-23 22:14:53 +0530

    LeetCode problems

---
+++ C++/Problem/twoSum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+public:
+    vector<int> twoSum(vector<int>&nums,int target){
+        vector<int> result;
+        for(auto it1=nums.begin(); it!=nums.end();++it){
+            auto it2=find(it+1,nums.end(),target-*it);
+            if()
+        }
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
