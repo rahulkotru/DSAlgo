8851db653c6d6f02aa63e9f77276cc38c1788589
Rahul <rahulkotr1@gmail.com>
2022-02-24 22:26:43 +0530

    Iterate island

--- Java/Problem/islandGraph.java
+++ Java/Problem/islandGraph.java
@@ -3,6 +3,7 @@ class islandGraph{
 
 
         public static int countIslands(char [][] island){
+            int count=0;
             for(int i=0;i<island.length;i++){
                 for(int j=0;j<island[i].length;j++){
                     if(island[i][j]=='1'){
@@ -11,5 +12,7 @@ class islandGraph{
                 }
             }
         }
+        return count;
     }
+    
 }
