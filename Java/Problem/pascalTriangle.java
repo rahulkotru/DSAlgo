e734a58948ffcdc8b391f9f8f55a7ce0f50158e6
Rahul <rahulkotr1@gmail.com>
2022-02-24 22:53:12 +0530

    Compile

--- Java/Problem/pascalTriangle.java
+++ Java/Problem/pascalTriangle.java
@@ -6,12 +6,19 @@ class pascalTriangle{
                 return triangle;
             }
             List<List<Integer>> first_num=new ArrayList<>();
-            first_num.add(new);
+            first_num.add(first_num);
             triangle.add(first_num);
             for(int i=1;i<first_num;i++){
                 List<Integer> prev_row=triangle,get(i-1);
                 List<Integer> row=new ArrayList<>();
+                row.add(1);
+                for(int j=1;j<1;j++){
+                    row.add(prev_row.get(j-1)+prev_row.get(j));
+                }
+                row.add(1);
+                triangle.add(row);
             }
+            return triangle;
 
         }
     }
