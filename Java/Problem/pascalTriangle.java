8d765cfeaaf5f1e2a087fed2c0f1f1c852512175
Rahul <rahulkotr1@gmail.com>
2022-02-24 22:45:08 +0530

    Initialize number

--- Java/Problem/pascalTriangle.java
+++ Java/Problem/pascalTriangle.java
@@ -1,8 +1,14 @@
 class pascalTriangle{
     class solution{
         public List <List<Integer>> triangle(int num){
-            List<List<Integer>>=new ArrayList<>();
-            if
+            List<List<Integer>> triangle=new ArrayList<>();
+            if(num==0){
+                return triangle;
+            }
+            List<List<Integer>> first_num=new ArrayList<>();
+            first_num.add(new);
+            triangle.add(first_num);
+
 
         }
     }
