775559e8dfe37389e8d4c59ebff6d11b73bd8cb8
Rahul <rahulkotr1@gmail.com>
2022-02-24 22:38:06 +0530

    Pascals Triangle

---
+++ Java/Problem/pascalTriangle.java
@@ -0,0 +1,9 @@
+class pascalTriangle{
+    class solution{
+        public List <List<Integer>> triangle(int num){
+            List<List<Integer>>=new ArrayList<>();
+            if
+
+        }
+    }
+}
