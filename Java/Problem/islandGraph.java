bf5545e5267f0c3d41603cdfeb81f02795c6f700
Rahul <rahulkotr1@gmail.com>
2022-02-24 22:18:30 +0530

    Iterate island

--- Java/Problem/islandGraph.java
+++ Java/Problem/islandGraph.java
@@ -2,8 +2,14 @@ class islandGraph{
     class Solution{
 
 
-        public static void countIslands(char [][] island){
-            
+        public static int countIslands(char [][] island){
+            for(int i=0;i<island.length;i++){
+                for(int j=0;j<island[i].length;j++){
+                    if(island[i][j]=='1'){
+                        callBFS(island,i,j);
+                    }
+                }
+            }
         }
     }
 }
