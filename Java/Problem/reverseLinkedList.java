1a8218ad3ba752f5c98d64053cff42c7c0c6f3b9
Rahul <rahulkotr1@gmail.com>
2022-02-25 21:46:06 +0530

    Reverse linkedlist

---
+++ Java/Problem/reverseLinkedList.java
@@ -0,0 +1,29 @@
+class reverseLinkedList{
+    private ListNode;
+    class ListNode{
+        ListNode next;
+        int data;
+        ListNode(int val){
+            data=val;
+        }
+    }
+    class Solution{
+        ListNode reverseLinkedList(ListNode head){
+            
+            ListNode prev;
+            ListNode current=head.next;
+            while(head.next!=null){
+                ListNode next=head.next;
+                current.next=prev;
+                prev=head;
+                current=next; 
+
+                
+            }
+            
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
