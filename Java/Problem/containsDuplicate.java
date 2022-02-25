fb066817d7f8cb5d561f715c58095b1c6cc0d2a0
Rahul <rahulkotr1@gmail.com>
2022-02-25 20:44:37 +0530

    Contains Duplicate

---
+++ Java/Problem/containsDuplicate.java
@@ -0,0 +1,13 @@
+class containsDuplicate{
+    class Solution{
+        public boolean containDuplicate(int [] nums){
+            HashSet<Integer> numbers=new HashSet<>();
+            for(int i=0;i<nums.length;i++){
+                
+            }
+        }
+    }
+    public void main(String [] args]){
+
+    }
+}
