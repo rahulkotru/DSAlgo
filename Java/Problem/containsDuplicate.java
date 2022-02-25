ffe08eea09bdb96df14d26620de5584f974b2b4b
Rahul <rahulkotr1@gmail.com>
2022-02-25 20:46:06 +0530

    Initialize method using HashSet

--- Java/Problem/containsDuplicate.java
+++ Java/Problem/containsDuplicate.java
@@ -3,8 +3,12 @@ class containsDuplicate{
         public boolean containDuplicate(int [] nums){
             HashSet<Integer> numbers=new HashSet<>();
             for(int i=0;i<nums.length;i++){
-                
+                if(numbers.contains(nums[i])){
+                    return true;
+                }
+                numbers.add(nums[i]);
             }
+            return false;
         }
     }
     public void main(String [] args]){
