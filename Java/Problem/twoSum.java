028742e0b3d1ace92d12707a3a98d851144a00fb
Rahul <rahulkotr1@gmail.com>
2022-02-25 22:07:45 +0530

    twoSum pointer initializes

--- Java/Problem/twoSum.java
+++ Java/Problem/twoSum.java
@@ -11,7 +11,24 @@ class twoSum{
                 data=val;
             }
         }
-        
+        public int [] twoSum(int [] arr, int target){
+            int pointer1,pointer2;
+            pointer1=0;
+            pointer2=arr.length-1;
+            while(pointer1<pointer2){
+                if(arr[pointer1]+arr[pointer2]>target){
+                    pointer2=-1;
+                }
+                else if(arr[pointer1]+arr[pointer2]<target){
+                    pointer1=+1;
+                }
+                else if(arr[pointer1]+arr[pointer2]==target){
+                    return new int[]{pointer1+1,pointer2+1};
+                }
+                
+                }
+            }return new int[]{pointer1+1,pointer2+1};
+        }
 
     }
     public static void main(String args[]){
