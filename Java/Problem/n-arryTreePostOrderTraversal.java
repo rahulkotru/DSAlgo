36779eecfb689013cb787cc3ab7f9324a8f930bf
Rahul <rahulkotr1@gmail.com>
2022-02-26 23:16:16 +0530

    ListNode defined

--- Java/Problem/n-arryTreePostOrderTraversal.java
+++ Java/Problem/n-arryTreePostOrderTraversal.java
@@ -1,5 +1,13 @@
 class n-arryTreePostOrderTraversal{
+    class Node{
+        int val;
+        List<Node> children;
+        Node(int data,List<Node> child){
+            this.val=data;
+            this.children=child;
+        }
+    }
     public static void main(String args[]){
-        
+
     }
 }
