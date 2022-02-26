1b0ed81695aec6aba0071ce67e48b910d53efab8
Rahul <rahulkotr1@gmail.com>
2022-02-26 22:55:43 +0530

    Instantiate linkedlist

--- Java/Problem/palindromeCheck.java
+++ Java/Problem/palindromeCheck.java
@@ -1,5 +1,20 @@
 class palindromeCheck{
+    class Solution {
+        class ListNode{
+            ListNode next;
+            int data;
+            ListNode(int val){
+                this.data=val;
+                this.next=null;
+            }
+            public boolean isPalindrome(ListNode head){
+                
+            }
+        }    
+    
+    
+    }
     public static void main(String[] args){
-        
+
     }
 }
