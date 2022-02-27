056b415977fd942ab70d7c467d78cf0fe73b6f38
Rahul <rahulkotr1@gmail.com>
2022-02-27 21:24:01 +0530

    Inorder Tree traversal

--- Java/Problem/n-arryTreeInorderTraversal.java
+++ Java/Problem/n-arryTreeInorderTraversal.java
@@ -10,6 +10,20 @@ class n-arryTreeInorderTraversal{
     public List<Integer> inorderTraversal(TreeNode root){
         Stack <TreeNode> stack=new Stack<>();
         List<Integer> output=new ArrayList<>();
+        if(root==null){
+            return output;
+        }
+        TreeNode current=root;
+        while(current!=null||stack.isEmpty()){
+            while(current!=null){
+                stack.push(current);
+                current=current.left;
+            }
+            current=stack.pop();
+            output.add(current.val);
+            current=current.right;
+        }
+        return output;
     }
     public static void main(String[] args){
 
