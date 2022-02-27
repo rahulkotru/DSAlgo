592ba67349124b54ab1071b62394838525335ffc
Rahul <rahulkotr1@gmail.com>
2022-02-27 21:47:23 +0530

    PreOrder traversal

--- Java/Problem/n-arryTreePreOrderTraversal.java
+++ Java/Problem/n-arryTreePreOrderTraversal.java
@@ -1,3 +1,16 @@
 class n-arryTreePreorderTraversal{
-    
+    class Node{
+        int val;
+        List<Node> children;
+        Node(int value, List<Node> child){
+            this.val=value;
+            this.children=child;
+        }
+
+    }
+    class Solution {
+        public List<Integer> preorder(Node root){
+            
+        }
+    }
 }
