c3c8140982955fadd416342a0e1b080a6ed3d854
Rahul <rahulkotr1@gmail.com>
2022-03-01 21:24:04 +0530

    Reverse Integer

--- Java/Problem/reverseInt.java
+++ Java/Problem/reverseInt.java
@@ -4,7 +4,10 @@ class reverseInt{
         int pop;
         int x=123;
         while(x!=0){
-            
+            pop=x%10;
+            x/=10;
+            reversed=(reversed*10)+pop;
         }
+        System.out.println(reversed);
     }
 }
