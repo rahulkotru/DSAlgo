387679383234ffed0e547b6a21f8ade4cdf8682a
Rahul <rahulkotr1@gmail.com>
2022-03-01 21:32:04 +0530

    Max depth Binary tree

--- Java/Problem/maxDepthBinaryTree.java
+++ Java/Problem/maxDepthBinaryTree.java
@@ -2,7 +2,14 @@ class maxDepthBinaryTree{
     class Solution {
         private int answer;
         void maxDepth(TreeNode root,int depth){
-            
+            if(root==null){
+                return 0;
+            }
+            if(root.left==null&&root.right==null){
+                answer=Math.max(answer,depth);
+            }
+            maxDepth(root.left,depth+1);
+            maxDepth(root.right,depth+1);
         }
     }
 }
