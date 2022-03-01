6e7f6b62d92d08d98d731ea56e1df5ebc8019b82
Rahul <rahulkotr1@gmail.com>
2022-03-01 21:46:32 +0530

    Symmetric Tree

---
+++ Java/Problem/symmetricTree.java
@@ -0,0 +1,25 @@
+class SymmetricTree{
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data){
+            this.val=data;
+        }
+    }
+    class Solution{
+        public boolean isSymmetric(TreeNode root){
+            return isMirror(root,root);
+        }
+        public boolean isMirror(TreeNode r1,TreeNode r2){
+            if(r1==null && r2==null){
+                return true;
+            }
+            if(r1==null||r2==null){
+                return false;
+            }
+            return(t1.val==t2.val)&& isMirror(r1.left,r2.right)&&
+        }
+
+    }
+}
