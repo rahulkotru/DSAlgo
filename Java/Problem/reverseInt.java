dcb032e8d8e0b50214f8c097a2d4c4f52d383fb2
Rahul <rahulkotr1@gmail.com>
2022-03-01 21:21:57 +0530

    Reverse Integer

--- Java/Problem/reverseInt.java
+++ Java/Problem/reverseInt.java
@@ -1,5 +1,10 @@
 class reverseInt{
     public static void main(String[] args){
-        
+        int reversed=0;
+        int pop;
+        int x=123;
+        while(x!=0){
+            
+        }
     }
 }
