cdc210f55c9bf95e2db444014cc515c5432de326
Rahul <rahulkotr1@gmail.com>
2022-03-02 23:11:59 +0530

    OddEven in place list reshuffle

--- Java/Problem/oddEvenList.java
+++ Java/Problem/oddEvenList.java
@@ -1,3 +1,27 @@
 class oddEvenList{
-    
+    class ListNode{
+        int data;
+        ListNode next;
+        ListNode(int val){
+            this.data=val;
+        }
+    }
+    public ListNode oddEven(ListNode root){
+        if(root==null){
+            return root;
+        }
+        
+        ListNode odd=root;
+        ListNode even=head.next;
+        ListNode evenHead=even;
+        while(even.next!=null&&even!=null){
+        odd.next=even.next;
+        odd=even.next;
+        even.next=odd.next;
+        even=even.next;
+        }
+        odd.next=evenHead;
+        return head;
+
+    }
 }
