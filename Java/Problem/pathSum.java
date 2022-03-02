e716f22ea66ef9b5cfcaae6c19d69b48bb5dee4c
Rahul <rahulkotr1@gmail.com>
2022-03-02 22:04:00 +0530

    pathSum

--- Java/Problem/pathSum.java
+++ Java/Problem/pathSum.java
@@ -1,3 +1,11 @@
 class pathSum{
-    
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data){
+            this.val=data;
+        }
+    }
+    class Solution
 }
