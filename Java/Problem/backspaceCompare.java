031e08b6b5e92f21dbf8a0c72f4d41b7d616dda9
Rahul <rahulkotr1@gmail.com>
2022-03-03 23:16:55 +0530

    backspaceCompare

--- Java/Problem/backspaceCompare.java
+++ Java/Problem/backspaceCompare.java
@@ -1,3 +1,24 @@
 class backspaceCompare{
-    
+    public boolean backspaceCompare(String s, String T){
+        int s_pointer=S.length()-1;
+        int t_pointer=T.length()-1;
+
+        int s_skips=0;
+        int t_skips=0;
+        while(s_pointer>=0||t_pointer<=0){
+            while(s_pointer>=0){
+                if(s.charAt(s_pointer)=='#'){
+                    s_skips+=1;
+                    s_pointer-=1;
+                }
+                else if(s_skips>0){
+                    s_pointer-=1;
+                    s_skips=-1;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
 }
