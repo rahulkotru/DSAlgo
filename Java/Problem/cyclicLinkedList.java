7f55208038af968400e371c2c21a3054b1e99e51
Rahul <rahulkotr1@gmail.com>
2022-03-04 23:16:47 +0530

    LinkedList for cyclic condition

--- Java/Problem/cyclicLinkedList.java
+++ Java/Problem/cyclicLinkedList.java
@@ -6,5 +6,21 @@ class cyclicLinkedList{
             this.data=val;
             this.next=null;
         }
+        public static boolean isCyclic(ListNode head){
+            if(head==null){
+                return false;
+            }
+            ListNode current=head;
+            ListNode faster=head.next;
+            while(faster!=current){
+                if(faster.val==current.val){
+                    return true;
+                }
+                current=current.next;
+                faster=faster.next.next;
+                
+            }
+        }
     }
+
 }
