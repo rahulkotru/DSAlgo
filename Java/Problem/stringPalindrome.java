8d764113cfdb578b38332b252c48fd41799dcea2
Rahul <rahulkotr1@gmail.com>
2022-03-04 23:42:57 +0530

    String Palindrome

---
+++ Java/Problem/stringPalindrome.java
@@ -0,0 +1,14 @@
+class stringPalindrome{
+    public static boolean isPalindrome(String s){
+        String fixed="";
+        for (char c:s.toCharArray()){
+            if(Character.isDigit(c)||Character.isLetter(c)){
+                fixed+=c;
+            }
+            fixed=fixed.toLowerCase();
+            int start=0;
+            
+
+        }
+    }
+}
