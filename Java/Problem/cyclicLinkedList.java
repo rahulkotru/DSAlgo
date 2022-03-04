fef544476fd6bbe5aa7af979ae04883f35297e2d
Rahul <rahulkotr1@gmail.com>
2022-03-04 23:08:14 +0530

    LinkedList for cyclic condition

--- Java/Problem/cyclicLinkedList.java
+++ Java/Problem/cyclicLinkedList.java
@@ -1,7 +1,10 @@
 class cyclicLinkedList{
     class ListNode{
         int data;
-        ListNode head;
-        ListNode(int data)
+        ListNode next;
+        ListNode(int val){
+            this.data=val;
+            this.next=null;
+        }
     }
 }
