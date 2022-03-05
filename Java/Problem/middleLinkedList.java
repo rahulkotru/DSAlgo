3232bd21e08288878163e00aac059b00473af6d6
Rahul <rahulkotr1@gmail.com>
2022-03-05 22:18:17 +0530

    Middle of linkedList

--- Java/Problem/middleLinkedList.java
+++ Java/Problem/middleLinkedList.java
@@ -1,3 +1,10 @@
 class middleLinkedList{
-    
+    class ListNode{
+        int data;
+        ListNode next;
+        ListNode(int val){
+            this.data=val;
+            this.next=null;
+        }
+    }
 }
