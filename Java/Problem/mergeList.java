608d4809dc8fcbe03f6208fd9401ffa57e89be6e
Rahul <rahulkotr1@gmail.com>
2022-03-05 22:25:53 +0530

    MergeList

--- Java/Problem/mergeList.java
+++ Java/Problem/mergeList.java
@@ -1,3 +1,13 @@
 class mergeList{
-    
+    class ListNode{
+        int data;
+        ListNode next;
+        ListNode(int val){
+            this.data=val;
+            this.next=null;
+        }
+    }
+    class Solution{
+        
+    }
 }
