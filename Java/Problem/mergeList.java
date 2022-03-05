03e02073c4a1466649f6ac2376498919c7a32b09
Rahul <rahulkotr1@gmail.com>
2022-03-05 22:28:44 +0530

    MergeList

--- Java/Problem/mergeList.java
+++ Java/Problem/mergeList.java
@@ -9,7 +9,17 @@ class mergeList{
     }
     class Solution{
         public static ListNode mergeTwo(ListNode List1,ListNode List2){
-            
+            if(List1 == null){
+                return List2;
+            }
+            if(List2 == null){
+                return List1;
+            }
+            ListNode point1=List1;
+            ListNode point2=List2;
+            while(point1.next!=null||point2.next!=null){
+                
+            }
         }
     }
 }
