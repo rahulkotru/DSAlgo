bb47807bc9b99c30c0d4a378743392a744c60ac1
Rahul <rahulkotr1@gmail.com>
2022-03-06 21:53:57 +0530

    Room key search

--- Java/Problem/roomKey.java
+++ Java/Problem/roomKey.java
@@ -1,6 +1,10 @@
 class roomKey{
     class Solution{
-        public boolean canVisitRoom(List<List<Integer>>){
+        public boolean canVisitRoom(List<List<Integer>> rooms){
+            boolean[] seen= new boolean[rooms.size()];
+            seen[0]=true;
+            Stack<Integer> keys=new Stack();
+            keys.add(0);
             
         }
     }
