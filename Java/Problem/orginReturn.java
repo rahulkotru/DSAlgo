e6adf81b67d159ce0667e985872fd3b5f741c753
Rahul <rahulkotr1@gmail.com>
2022-03-06 21:39:54 +0530

    Robot moving through grid

--- Java/Problem/orginReturn.java
+++ Java/Problem/orginReturn.java
@@ -1,7 +1,11 @@
 class originReturn{
     class Solution {
         public boolean moveRobot(String moves){
-            
+            int x=0;
+            int y=0;
+            for(char move: moves.toCharArray()){
+                
+            }
         }
     }
 }
