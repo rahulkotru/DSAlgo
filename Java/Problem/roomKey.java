f2de0c80ba73a84899736fbf1383d116d5dd5906
Rahul <rahulkotr1@gmail.com>
2022-03-06 21:56:40 +0530

    Room key search

--- Java/Problem/roomKey.java
+++ Java/Problem/roomKey.java
@@ -5,7 +5,19 @@ class roomKey{
             seen[0]=true;
             Stack<Integer> keys=new Stack();
             keys.add(0);
-            
+            while(!keys.isEmpty()){
+                int current_keys=keys.pop();
+                for(int new_key:rooms.get(current_keys)){
+                    if(!seen[new_key]){
+                        seen[new_key]=true;
+                        keys.add(new_key);
+                    }
+                }
+            }
+            for(boolean visited:seen){
+                if(!visited) return false;
+            }
+            return true;
         }
     }
 }
