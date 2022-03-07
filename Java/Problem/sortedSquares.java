00a52eb83ac032c2f642ed8b4871aa515a9424ea
Rahul <rahulkotr1@gmail.com>
2022-03-07 23:37:12 +0530

    Sorted Squares

--- Java/Problem/sortedSquares.java
+++ Java/Problem/sortedSquares.java
@@ -7,6 +7,13 @@ class sortedSquares{
                 positive_pointer+=1;
             }
             int negative_pointer=positive_pointer-1;
+            int [] Sorted_Squares=new int[N];
+            int counter=0;
+            while(negative_pointer>=0&&positive_pointer<N){
+                if(A[negative_pointer]*A[negative_pointer]<A[positive_pointer]*A[positive_pointer]){
+                    
+                }
+            }
         }
     }
 }
