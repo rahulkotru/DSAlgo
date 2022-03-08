c16ac811cbc9a59db6babdc623c07900669b7214
Rahul <rahulkotr1@gmail.com>
2022-03-08 22:06:47 +0530

    Max water

--- Java/Problem/mostWater.java
+++ Java/Problem/mostWater.java
@@ -1,7 +1,17 @@
 class mostWater{
     class Solution{
         public int maxArea(int[] height){
-            
+            int max_area=0;
+            int start=0;
+            int end=height.length-1;
+            while(start<end){
+                if(height[start]<height[end]){
+                    max_area=Math.max(max_area, height[start]*height[end]);
+                    start++;
+                }
+                else()
+
+            }
         }
     }
 }
