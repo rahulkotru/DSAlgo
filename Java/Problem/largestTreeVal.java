ff58b81996a1717d3511c258925c46bc2a46047d
Rahul <rahulkotr1@gmail.com>
2022-03-09 21:44:05 +0530

    Define treenode

--- Java/Problem/largestTreeVal.java
+++ Java/Problem/largestTreeVal.java
@@ -1,3 +1,12 @@
 class largestTreeVal{
-    
+    class Solution {
+        class TreeNode{
+            TreeNode left;
+            TreeNode right;
+            int val;
+            TreeNode (int data){
+                this.val=data;
+            }
+        }   
+    }
 }
