64583fe07e9a8e606d953c708282e88c0fe7ff27
Rahul <rahulkotr1@gmail.com>
2022-03-09 21:52:23 +0530

    Largest value at each tree level

--- Java/Problem/largestTreeVal.java
+++ Java/Problem/largestTreeVal.java
@@ -1,12 +1,28 @@
 class largestTreeVal{
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode (int data){
+            this.val=data;
+        }
+    } 
     class Solution {
-        class TreeNode{
-            TreeNode left;
-            TreeNode right;
-            int val;
-            TreeNode (int data){
-                this.val=data;
+        
+        public List<Integer> maxLevel(TreeNode root){
+            List<Integer> largestVal=new ArrayList();
+            helper(root, largestVal,0);
+            return largestVal;
+        }  
+        public void helper(TreeNode root, List<Integer> largestVal,int level){
+            if(root==null) return;
+            if(level==largestVal.size()){
+                largestVal.add(root.val);
             }
-        }   
+            else{
+                largestVal.set(level, Math.max)
+            }
+        }
+    
     }
 }
