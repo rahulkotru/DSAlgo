9777368754bf86169275062812661521a4079409
Rahul <rahulkotr1@gmail.com>
2022-03-09 22:02:03 +0530

    Binary Tree Pruning

--- Java/Problem/binaryTreePruning.java
+++ Java/Problem/binaryTreePruning.java
@@ -1,3 +1,13 @@
 class binaryTreePruning{
-    
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int data){
+            this.val=data;
+        }
+    }
+    class Solution{
+        
+    }
 }
