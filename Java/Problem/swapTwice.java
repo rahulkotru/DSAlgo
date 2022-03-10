7f0d263ce4a0c32e7c5d4bd0007b826d1e65134e
Rahul <rahulkotr1@gmail.com>
2022-03-10 20:41:20 +0530

    Swapping Twice in a ListNode

--- Java/Problem/swapTwice.java
+++ Java/Problem/swapTwice.java
@@ -18,7 +18,11 @@ class swapTwice{
                 ListNode first=current.next;
                 ListNode second=current.next.next;
                 first.next=second.next;
+                current.next=second;
+                current.next.next=first;
+                current=current.next.next;
             }
+            return temp.next;
         }
     }
 }
