9238fb76837d033b5dd53411ffa64fd97159171d
Rahul <rahulkotr1@gmail.com>
2022-03-10 20:15:26 +0530

    TreeMap handOfStraight

--- Java/Problem/handOfStraight.java
+++ Java/Problem/handOfStraight.java
@@ -1,6 +1,15 @@
 class handOf Straight{
     class Solution {
         public boolean straightEdge(int [] cards, int W){
+            TreeMap<Integer, Integer> cardCounts=new TreeMap();
+            for(int card :cards ){
+                if(!cardCounts.containsKey(card)){
+                    cardCounts.put(card,1);
+                }
+                else{
+                    cardCounts.replace(card,cardCounts.get(card)+1);
+                }
+            }
             
         }
     }
