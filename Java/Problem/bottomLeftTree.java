27b3dbe5bf1e26a96d1252a065634d02dc39b3da
Rahul <rahulkotr1@gmail.com>
2022-03-11 23:21:01 +0530

    Bottom tree traversal"

--- Java/Problem/bottomLeftTree.java
+++ Java/Problem/bottomLeftTree.java
@@ -7,5 +7,16 @@ class bottomLeftTree{
             this.val=data;
         }
     }
-    
+    class Solution{
+        public int findBottomLeft(TreeNode root){
+            Queue<TreeNode> queue=new LinkedList();
+            queue.offer(root);
+            while(!queue.isEmpty()){
+                root=queue.poll();
+                if(root.right!=null){
+                    queue.offer(root.right);
+                }
+            }
+        }
+    }
 }
