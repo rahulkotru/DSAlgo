fae370dc27b871601672f5aa480a9f02a8ad7d3a
Rahul <rahulkotr1@gmail.com>
2022-03-12 22:18:51 +0530

    Sum in range of Binary search

--- Java/Problem/sumBST.java
+++ Java/Problem/sumBST.java
@@ -3,5 +3,13 @@ class sumBST{
         int val;
         TreeNode left;
         TreeNode right;
+        TreeNode( int data){
+            this.val=data;
+        }
+    }
+    class Solution {
+     public int sumIterative(TreeNode root, int L, int R){
+         
+     }   
     }
 }
