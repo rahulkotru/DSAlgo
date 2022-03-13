273d37474f4a85edb00d774852a4ba61658877a7
Rahul <rahulkotr1@gmail.com>
2022-03-13 22:23:30 +0530

    Give coins to a binary tree(DFS)

--- Java/Problem/giveCoins.java
+++ Java/Problem/giveCoins.java
@@ -1,3 +1,16 @@
 class giveCoins{
-    
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int data){
+            this.val=data;
+        }
+    }
+    class Solution{
+        int coins;
+        public static int distributeCoins(TreeNode root){
+            
+        }
+    }
 }
