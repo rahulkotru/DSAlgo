975ead74ff391fea44847c24af3cea1348ba1a37
Rahul <rahulkotr1@gmail.com>
2022-04-05 22:13:48 +0530

    Completeness of Binary Tree

--- Java/Problem/CompleteBinaryTree.java
+++ Java/Problem/CompleteBinaryTree.java
@@ -1,5 +1,13 @@
 public class CompleteBinaryTree{
+    public class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+        TreeNode(int data){
+            this.data=data;
+        }
+    }
     public static void main (String args[]){
-        
+
     }
 }
