c2f15e32751589d64bd3d1b22882ebfeacbbe090
Rahul <rahulkotr1@gmail.com>
2022-04-08 23:46:48 +0530

    Width of Binary Tree

--- Java/Problem/maxWidthBinaryTree.java
+++ Java/Problem/maxWidthBinaryTree.java
@@ -10,8 +10,20 @@ class maxWidthBinaryTree{
     public class Solution {
         int maxWidth;
         HashMap<Integer, Integer> leftmostPosition;
-        public int width(TreeNode root){
-            
+        public int searchWidth(TreeNode root){
+            int max=0;
+            leftmostPosition=new Hashmap();
+            getWidth(root, 0, 0);
+            return max;
+        }
+
+        public void getWidth(TreeNode root, int depth, int position){
+            if(root==null)return 0;
+            leftmostPosition.computeIfAbsent(root, x->position);
+            max=Math.max(max,position-leftmostPosition.get(depth)-1);
+            getWidth(root.left,depth+1,position*2);
+            getWidth(root.right,depth+1,position*2+1);
+             
         }
         public static void main(String[] args){
             
