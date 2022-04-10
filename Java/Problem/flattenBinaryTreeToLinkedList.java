58b7c9eb128e7008cee87c4a31eed40ccfac3d96
Rahul <rahulkotr1@gmail.com>
2022-04-10 23:11:00 +0530

    Flatten Tree to LinkedList

--- Java/Problem/flattenBinaryTreeToLinkedList.java
+++ Java/Problem/flattenBinaryTreeToLinkedList.java
@@ -11,7 +11,16 @@ class flattenBinaryTreeToLinkedList{
         public void flatten(TreeNode root){
             if(root==null) return
             Stack<TreeNode> stack=new Stack();
-            
+            stack.push(root);
+            while(!stack.isEmpty){
+                TreeNode current=stack.pop();
+                if(current.right!=null){
+                    stack.push(current.right);
+                }
+                if(current.left!=null){
+                    stack.push(current.left);
+                }
+            }
         }
     }
 }
