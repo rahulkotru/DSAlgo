7eac0867d498351ed7d1332032e7563fd43fd183
Rahul <rahulkotr1@gmail.com>
2022-04-13 22:03:52 +0530

    Flip Image matrix

--- Java/Problem/flipImage.java
+++ Java/Problem/flipImage.java
@@ -1,4 +1,12 @@
 class flipImage{
+    class Solution{
+        public int [][] flip(int [][] arr){
+            for(int i= 0;i<arr.length;i++){
+                int start=0;
+                int end=arr.length-1;
+            }
+        }
+    }
     public static void main(String[] args){
         
     }
