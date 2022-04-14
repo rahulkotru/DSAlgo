b69a8b9941970831db0d4f404a45ff0b82cd11d8
Rahul <rahulkotr1@gmail.com>
2022-04-14 22:33:37 +0530

    Valid palindrome

--- Java/Problem/validPalindrome.java
+++ Java/Problem/validPalindrome.java
@@ -13,7 +13,16 @@ class validPalindrome{
             reutrn true;
         }
         public boolean helper(String s, int i, int j){
-            
+            int start=i;
+            int end=j;
+            while(i<j){
+                if(s.charAt(i)!=s.charAt(j)){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
         }
     }
     public static void main(String args[]){
