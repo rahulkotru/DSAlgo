e9ca84f65643c8f3214fba7c2632ab1436260ac3
Rahul <rahulkotr1@gmail.com>
2022-04-14 22:00:56 +0530

    Flip Image matrix

--- Java/Problem/rotateImage.java
+++ Java/Problem/rotateImage.java
@@ -1,30 +1,36 @@
 class rotateImage{
-    class Solution{
-        public void rotate(int [][] arr){
-            int start=0;
-            int end=arr[0].length;
-            for(int i=0;i<arr.length;i++){
-                for(j=0;j<arr[0].length;j++){
-                    arr[i][j]=arr[j][i]
-                }
+    public static class Solution{
+        
+    }
+    public static void rotate(int [][] arr){
+        int start=0;
+        int end=arr[0].length-1;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr[0].length;j++){
+                if(arr[i][j]!=arr[j][i])
+                arr[i][j]=arr[j][i];
             }
-            for(int i=0;i<arr.length;i++){
-                int temp=arr[i][start];
-                arr[i][start]=arr[i][end];
-                arr[i][end]=temp;
-            }
-            print(arr);
         }
-        public static void print(arr[][]){
-            for(int i=0;i<arr.length;i++){
-                for(j=0;j<arr[0].length;j++){
-                    System.out.println(arr[i][j]+'');
-                }
+        
+        for(int i=0;i<arr.length;i++){
+            int temp=arr[i][start];
+            arr[i][start]=arr[i][end];
+            arr[i][end]=temp;
+        }
+        print(arr);
+    }
+    public static void print(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.println(arr[i][j]);
             }
         }
     }
     public static void main(String[] args){
-        int arr[][]={{1,2,3},{4,5,6}};
-        System.out.println(arr[0].length);
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        //System.out.println(arr.length);
+        //System.out.println(arr[0].length);
+        rotate(arr);
+        //System.out.println(arr[0].length);
     }
 }
