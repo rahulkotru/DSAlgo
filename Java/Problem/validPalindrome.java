a46d3f2617daf3148c9f57411b520f6dcfa0497b
Rahul <rahulkotr1@gmail.com>
2022-04-14 22:28:33 +0530

    Valid palindrome

--- Java/Problem/validPalindrome.java
+++ Java/Problem/validPalindrome.java
@@ -1,8 +1,10 @@
 class validPalindrome{
     class Solution{
-
+        public boolean validate(String s){
+            
+        }
     }
     public static void main(String args[]){
-        
+
     }
 }
