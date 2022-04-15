d07c8687d10eb0edf99e67f90d572275b833f03d
Rahul <rahulkotr1@gmail.com>
2022-04-15 22:19:06 +0530

    Search Binary Tree

--- Java/Problem/searchBinaryTree.java
+++ Java/Problem/searchBinaryTree.java
@@ -1,7 +1,12 @@
 class searchBinaryTree{
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
     class Solution{
         public TreeNode search(TreeNode root){
-            
+
         }
     }
 }
