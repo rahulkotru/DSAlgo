7ee681b7860a6946463437150df7f0bd5bd68b11
Rahul <rahulkotr1@gmail.com>
2022-04-16 23:30:48 +0530

    Transpose matrix

--- Java/Problem/arrayTranspose.java
+++ Java/Problem/arrayTranspose.java
@@ -7,6 +7,7 @@ class arrayTranspose{
                 for(j=i;j<row;j++){
                     int temp=arr[i][j];
                     arr[i][j]=arr[j][i];
+                    arr[j][i]=temp;
                 }
             }
         }
