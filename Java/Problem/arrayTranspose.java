bd00a50f8633110e519371ba4832de519a64d526
Rahul <rahulkotr1@gmail.com>
2022-04-16 23:28:24 +0530

    Transpose matrix

--- Java/Problem/arrayTranspose.java
+++ Java/Problem/arrayTranspose.java
@@ -1,10 +1,11 @@
 class arrayTranspose{
     class Solution{
         public static int [][] transpose(int [][] arr){
-
+            int row= arr.length();
+            
         }
     }
     public static void main(String[] args){
-        
+
     }
 }
