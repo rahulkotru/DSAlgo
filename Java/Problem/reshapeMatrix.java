69c53303cb93ea1a65c70f4d289868f547f1d311
Rahul <rahulkotr1@gmail.com>
2022-04-17 22:12:57 +0530

    Reshape matrix

--- Java/Problem/reshapeMatrix.java
+++ Java/Problem/reshapeMatrix.java
@@ -3,6 +3,9 @@ class reshapeMatrix{
         public int[][] matrixReshape(int [][] nums, int r, int c){
             int rows=nums.length;
             int cols=nums[0].length;
+
+            if(rows*cols!= r*c)return nums;
+            int [][] outputArr=0;
         }
     }
 }
