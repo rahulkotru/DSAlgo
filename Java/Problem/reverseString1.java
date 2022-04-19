072d6fcce190438be99b314b3256dc87ee75d679
Rahul <rahulkotr1@gmail.com>
2022-04-19 23:10:01 +0530

    Reverse String In Place

--- Java/Problem/reverseString1.java
+++ Java/Problem/reverseString1.java
@@ -1,10 +1,10 @@
 class reverseString1{
     class Solution{
         public static String reverse(String s){
-
+            if(s.length equals (0)) return s;
         }
     }
     public static void main(String[] args){
-        
+
     }
 }
