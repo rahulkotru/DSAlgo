2741c5b059201283010c29f80dadf0d4e3f2360a
Rahul <rahulkotr1@gmail.com>
2022-04-21 23:36:49 +0530

    Spiral Traversal

--- Java/Problem/spiralTraversal.java
+++ Java/Problem/spiralTraversal.java
@@ -2,6 +2,7 @@ class spiralTraversal{
     class Solution{
         public static List<Integer> spiral(int [][] arr){
             List <Integer> res=new ArrayList();
+            if(arr.length==0) return res;
         }
     }
     public static void main(String[]){
