16dafe9fdcf826a3a56e090026f1bc9fd6c1257a
Rahul <rahulkotr1@gmail.com>
2022-04-23 23:22:42 +0530

    spiral Matrix second iteration

--- Java/Problem/spiralMatrix2.java
+++ Java/Problem/spiralMatrix2.java
@@ -2,6 +2,8 @@ spiralMatrix2{
     class Solution{
         public int[][] spiral(int n){
             int [][] arr=int[n][n]
+            int rowBeg=0
+            int rowEnd=n-1
         }
     }
     public static void main(String[] ){
