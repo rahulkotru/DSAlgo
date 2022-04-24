a32c846fbeef08f0a74d125d9eae55df8197d819
Rahul <rahulkotr1@gmail.com>
2022-04-24 22:10:35 +0530

    Unique Email

--- Java/Problem/uniqueEmail.java
+++ Java/Problem/uniqueEmail.java
@@ -2,7 +2,11 @@ class uniqueEmail{
     class Solution {
         public int uniqueNames(String[] emails){
             HashSet<String> uniqueMail=new HashSet();
-            
+            for (String email: emails){
+                int splitPosition =email.indexOf("@");
+                String localeName=email.substring(0,splitPosition);
+                String domainName=email.substring(splitPosition);
+            }
         }
     }
     public static void main(String[]args){
