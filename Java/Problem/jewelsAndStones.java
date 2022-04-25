397e684c9ef6857285680301c2b040078487c3a4
Rahul <rahulkotr1@gmail.com>
2022-04-25 23:02:06 +0530

    Jewels and Stones

--- Java/Problem/jewelsAndStones.java
+++ Java/Problem/jewelsAndStones.java
@@ -2,7 +2,9 @@ class Solution{
     public static int numJewels(String J, String S){
         int numJewels=0;
         for(int i=0;i<S.length();i++){
-            if(J.indexOf(S.charAt(i))>-1){}
+            if(J.indexOf(S.charAt(i))>-1){
+                numJewels++;
+            }
         }
     }
 }
