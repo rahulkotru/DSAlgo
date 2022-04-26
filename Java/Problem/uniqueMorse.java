d8a45128bcf8b3beabdb3b4929cd2d300b561427
Rahul <rahulkotr1@gmail.com>
2022-04-26 23:12:51 +0530

    Unique Morse Code

--- Java/Problem/uniqueMorse.java
+++ Java/Problem/uniqueMorse.java
@@ -2,7 +2,10 @@ class uniqueMorse{
     class Solution {
         public int transformMorse(String [] words){
             HashSet<String> uniqueCodes=new HashSet();
-            String Morse[]=new String{""};
+            String Morse[]=new String[]{""};
+            for (word :words){
+                S
+            }
         }
     }
     public static void main(String[] args){
