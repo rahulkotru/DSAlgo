5f021d9a2f7d5700c3addeea796eb1522f56b26d
Rahul <rahulkotr1@gmail.com>
2022-04-27 23:17:44 +0530

    Single number

--- Java/Problem/singleNumber.java
+++ Java/Problem/singleNumber.java
@@ -1,8 +1,10 @@
 class singleNumber{
     class Solution{
         public int single(int arr[]){
-             int number=0;
-             for(int i=0;i<arr.length;)
+            int number=0;
+            for(int i=0;i<arr.length;i++){
+                
+            }
         }
     }
     public static void main(String args[]){
