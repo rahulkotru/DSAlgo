d5ecd46792bbea4b6aacfaa4ca55c00c20751b3f
Rahul <rahulkotr1@gmail.com>
2022-04-28 23:00:14 +0530

    Peak index of mountain

--- Java/Problem/peakIndexMountain.java
+++ Java/Problem/peakIndexMountain.java
@@ -2,7 +2,7 @@ class peakIndexMountain{
     class Solution{
         public static int peakIndex(int[] A){
             int left=0;
-            
+            int right=A.length-1;
         }
     }
     public static void main(String args[]){
