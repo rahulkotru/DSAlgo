25ead27c841ab0f4e69643aaed5cfd8cf2c80070
Rahul <rahulkotr1@gmail.com>
2022-04-29 23:20:43 +0530

    sum of left leaves

--- Java/Problem/sumOfLeftLeaves.java
+++ Java/Problem/sumOfLeftLeaves.java
@@ -1,3 +1,10 @@
 class sumOfLeftLeaves{
-    
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int data;
+        TreeNode(int val){
+            this.data = val;
+        }
+    }
 }
