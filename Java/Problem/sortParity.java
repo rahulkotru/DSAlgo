69f313b984a65ef81e56f6d79b957b7ac4a3be88
Rahul <rahulkotr1@gmail.com>
2022-04-30 23:08:17 +0530

    Sort Parity

--- Java/Problem/sortParity.java
+++ Java/Problem/sortParity.java
@@ -3,6 +3,12 @@ class sortParity{
         public int[] sortPair(int []A){
             int [] newArr=new int[A.length];
             int counter=0;
+            for (int i=0;i<A.length;i++){
+                if(A[i]%2==0){
+                    
+                }
+
+            }
         }
     }
     public static void main(String[] args){
