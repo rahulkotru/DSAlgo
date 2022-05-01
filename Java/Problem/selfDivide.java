6b3a550e12e1ae137a110d05dc5ae7baa97c96a4
Rahul <rahulkotr1@gmail.com>
2022-05-01 23:41:03 +0530

    self division

--- Java/Problem/selfDivide.java
+++ Java/Problem/selfDivide.java
@@ -1,8 +1,10 @@
 class selfDivide{
     class Solution{
-
+        public static isSelfDivide(int left, int right){
+            
+        }
     }
     public static void main(String [] args){
-        
+
     }
 }
