f7dbf6eed57b92bae386803bcd5f5288e37670b0
Rahul <rahulkotr1@gmail.com>
2022-05-02 23:04:04 +0530

    wordSubset

--- Java/Problem/wordSubset.java
+++ Java/Problem/wordSubset.java
@@ -1,7 +1,10 @@
 class wordSubset{
     class Solution {
         public List<String> words(String [] A, String [] B){
-            
+
         }
+    }
+    public static void main(String [] args){
+        
     }
 }
