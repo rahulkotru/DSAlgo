2be6e98783d4e1389581f6c3bb4efde27792275e
Rahul <rahulkotr1@gmail.com>
2022-05-03 23:18:46 +0530

    Array Partition

--- Java/Problem/arrayPartition.java
+++ Java/Problem/arrayPartition.java
@@ -1,7 +1,7 @@
 class arrayPartition{
     class Solution{
         public int arrPart(int [] nums){
-            
+            Arrays.sort(nums);
         }
     }
     public static void main(String [] args){
