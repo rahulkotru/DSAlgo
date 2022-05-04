7490ca200a169c5327519e7a7a70a0b7a1168a94
Rahul <rahulkotr1@gmail.com>
2022-05-04 23:32:22 +0530

    Common characters

--- Java/Problem/commonChars.java
+++ Java/Problem/commonChars.java
@@ -2,11 +2,14 @@ class commonChars{
     class Solution{
         public List<String> common(String arr[]){
             List<String> commonChars=new ArrayList();
-            
+            int [] minFreq=new int[26];
             Arrays.fill(minFreq,Integer.MAX_VALUE);
             for (String current: arr){
-                int [] minFreq=new int[26];
+                int [] charFreq=new int[26];
                 for (char c: current.toCharArray()){
+                    charFreq[c-'a']++;
+                }
+                for(int i=0;i<26;i++){
                     
                 }
             }
