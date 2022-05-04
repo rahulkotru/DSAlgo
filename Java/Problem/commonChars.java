31045175cb11ae20b33836d28705158a4e07de4b
Rahul <rahulkotr1@gmail.com>
2022-05-04 23:27:41 +0530

    Common characters

--- Java/Problem/commonChars.java
+++ Java/Problem/commonChars.java
@@ -1,7 +1,7 @@
 class commonChars{
     class Solution{
         public List<String> common(String arr[]){
-            
+            List<String> commonChars=new ArrayList();
         }
     }
     public static void main(String args[]){
