268153c36a96d62ef2e67cfda3ffacbeea801516
Rahul <rahulkotr1@gmail.com>
2022-05-05 22:57:44 +0530

    word BFS

--- Java/Problem/wordBFS.java
+++ Java/Problem/wordBFS.java
@@ -1,7 +1,7 @@
 class wordBFS{
     class Solution{
-        public List<String> wordle(String begin, String end, List<String> bagOWords){
-            
+        public int wordle(String begin, String end, List<String> bagOWords){
+
         }
     }
     public static void main(String args[]){
