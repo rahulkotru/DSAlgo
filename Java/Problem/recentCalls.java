19a34b9e8087b9f288241b56559f13f959687cf4
Rahul <rahulkotr1@gmail.com>
2022-05-06 22:22:56 +0530

    Recent call

--- Java/Problem/recentCalls.java
+++ Java/Problem/recentCalls.java
@@ -2,11 +2,13 @@ class recentCalls{
     Queue <Integer> queue;
     class Solution{
         public Solution(){
-
+            queue=new LinkedList();
         }
         public int ping(int t){
             queue.add(t);
-            while(queue.peek())
+            while(queue.peek()<t-3000){
+                queue.poll();    
+            }
             return queue.size();
         }
     }
