a2d9601a74071d3d4c47c14f83c0fd6af5a23752
Rahul <rahulkotr1@gmail.com>
2022-05-07 23:31:20 +0530

    Min size subarray

--- minSizeSubArray.java
+++ minSizeSubArray.java
@@ -2,6 +2,8 @@ class minSizeSubArray{
     class Solution{
         public int minSize(int a, int[] nums){
             int left=0;
+            int result=Integer.MAX_VALUE;
+            
         }
     }
     public static void main(String args[]){
