30d056e03d41d8add8ee65399d1820648e9de24d
Rahul <rahulkotr1@gmail.com>
2022-05-07 23:29:03 +0530

    Min size subarray

--- minSizeSubArray.java
+++ minSizeSubArray.java
@@ -1,8 +1,10 @@
 class minSizeSubArray{
     class Solution{
-
+        public int minSize(int a, int[] nums){
+            
+        }
     }
     public static void main(String args[]){
-        
+
     }
 }
