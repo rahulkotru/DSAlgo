245b9c8259b6922c2e822e894d3c3eee39f9f028
Rahul <rahulkotr1@gmail.com>
2022-05-09 22:55:47 +0530

    Sum of parity array

--- Java/Problem/sumParityArray.java
+++ Java/Problem/sumParityArray.java
@@ -1,7 +1,7 @@
 class sumParityArray{
     class Solution{
         public int[] sortArray(int [] A){
-            
+            int i=0;
         }
     }
     public static void main(String[] args){
