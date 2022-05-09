ae1995a838f5777a1b2d28a805ddd170849cab4d
Rahul <rahulkotr1@gmail.com>
2022-05-09 22:58:15 +0530

    Sum of parity array

--- Java/Problem/sumParityArray.java
+++ Java/Problem/sumParityArray.java
@@ -5,8 +5,11 @@ class sumParityArray{
             int j=1;
             int len=A.length;
             while(i<n&& j<n){
-                while(i<n&&A[i]%%2==0){
+                while(i<n&&A[i]%2==0){
                     i+=2;
+                }
+                while(j<n&& A[j]%2==1){
+                    j+=2;
                 }
             }
         }
