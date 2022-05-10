d6f670ec6a513ff2af06ad5d32b805426e70e800
Rahul <rahulkotr1@gmail.com>
2022-05-10 23:25:48 +0530

    Maximize profit in stock market

--- Java/Problem/maxProfit.java
+++ Java/Problem/maxProfit.java
@@ -4,7 +4,9 @@ class maxProfit{
             int minValue=Integer.MAX_VALUE;
             int maxProf=0;
             for (int i=0;i<prices.length;i++){
-                
+                if(prices[i]>minValue){
+                    
+                }
             }
         }
     }
