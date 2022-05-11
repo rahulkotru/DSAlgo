173d1194c4ca7623975a827e7903ea46991058a5
Rahul <rahulkotr1@gmail.com>
2022-05-11 23:41:12 +0530

    add two nums

--- Java/Problem/addTwoNums.java
+++ Java/Problem/addTwoNums.java
@@ -18,6 +18,7 @@ class addTwoNums{
             while(l1!=null||l2!=null){
                 int l1Val= (l1.data!=null)? l1.data:0;
                 int l2Va=(l2.data!=null)? l2.data:0;
+                int currentSum=l1Val+l2Val;
             }
         }
     }
