3f4313a73f4e859fb65d265d4bba3211312b515a
Rahul <rahulkotr1@gmail.com>
2022-05-11 23:30:36 +0530

    add two nums

--- Java/Problem/addTwoNums.java
+++ Java/Problem/addTwoNums.java
@@ -1,8 +1,10 @@
 class addTwoNums{
     class Solution{
-
+        public ListNode addTwoNums(ListNode l1, ListNode l2){
+            
+        }
     }
     public static void main(String args[]){
-        
+
     }
 }
