8ae7ea6d3dbd4d13c5b4a5853e1e37999e1ac0ff
Rahul <rahulkotr1@gmail.com>
2022-05-12 23:04:18 +0530

    Search 2D Matrix

--- Java/Problem/search2DMatrix.java
+++ Java/Problem/search2DMatrix.java
@@ -1,8 +1,10 @@
 class search2DMatrix{
     class Solution {
-
+        public boolean searchMatrix(int [][] matrix, int target){
+            
+        }
     }
     public static void main(String args[]){
-        
+
     }
 }
