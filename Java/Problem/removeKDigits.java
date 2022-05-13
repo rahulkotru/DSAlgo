f527ee97a025865372bd47974c7d7bdb110b79d2
Rahul <rahulkotr1@gmail.com>
2022-05-13 23:44:17 +0530

    Remove k digits

--- Java/Problem/removeKDigits.java
+++ Java/Problem/removeKDigits.java
@@ -3,6 +3,14 @@ class removeKDigits{
         public String removeDigits(String nums, int k){
             int size=nums.length;
             if(k==size) return '0';
+            Stack<Character> stack=new Stack();
+
+            int counter=0;
+            while(counter<size){
+                while(k>0&&!stack.isEmpty()&& stack.peek()>nums.charAt(counter)){
+                    
+                }
+            }
         }
 
     }
