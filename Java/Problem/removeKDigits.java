65f3beea8b7bd2d52ad98d1eb74219c7768b7d9f
Rahul <rahulkotr1@gmail.com>
2022-05-13 23:45:11 +0530

    Remove k digits

--- Java/Problem/removeKDigits.java
+++ Java/Problem/removeKDigits.java
@@ -8,8 +8,15 @@ class removeKDigits{
             int counter=0;
             while(counter<size){
                 while(k>0&&!stack.isEmpty()&& stack.peek()>nums.charAt(counter)){
-                    
+                    stack.pop();
+                    k--;
                 }
+                stack.push(nums.charAt(counter));
+                counter++;
+            }
+            while(k>0){
+                stack.pop();
+                k--;
             }
         }
 
