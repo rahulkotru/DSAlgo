254c069a1c09f4c191452bca8337bcd3c5fbeb24
Rahul <rahulkotr1@gmail.com>
2022-05-13 23:39:03 +0530

    Remove k digits

--- Java/Problem/removeKDigits.java
+++ Java/Problem/removeKDigits.java
@@ -1,8 +1,11 @@
 class removeKDigits{
     class Solution{
+        public String removeDigits(String nums, int k){
+            
+        }
 
     }
     public static void main(String[] args){
-        
+
     }
 }
