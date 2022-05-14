dd205510f3068d297c4416d5815656d523cd81c4
Rahul <rahulkotr1@gmail.com>
2022-05-14 23:26:48 +0530

    Word Search

--- Java/Problem/wordSearch.java
+++ Java/Problem/wordSearch.java
@@ -1,8 +1,10 @@
 class wordSearch{
     class Solution{
-
+        public boolean exists(char board[][],String word){
+            
+        }
     }
     public static void main(String[] args){
-        
+
     }
 }
