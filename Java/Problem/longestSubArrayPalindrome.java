7c5f01733b1e07977a0d49f88f60e745b6af6109
Rahul <rahulkotr1@gmail.com>
2022-05-15 23:51:46 +0530

    Longest Palindrome subarray

--- Java/Problem/longestSubArrayPalindrome.java
+++ Java/Problem/longestSubArrayPalindrome.java
@@ -10,6 +10,7 @@ class longestSubArrayPalindrome{
                 int len3=Math.max(len1,len2);
                 if(len>end-start){
                     start=i-((len-1)/2);
+                    end=i+(len/2);
                 }
             }
         }
