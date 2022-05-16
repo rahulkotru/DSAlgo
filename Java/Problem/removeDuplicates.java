077385a30aaf6418c386172b1c162f8964b856b7
Rahul <rahulkotr1@gmail.com>
2022-05-16 22:45:27 +0530

    remove Duplicate

--- Java/Problem/removeDuplicates.java
+++ Java/Problem/removeDuplicates.java
@@ -2,7 +2,10 @@ class removeDuplicate{
     class Solution{
         public String removeDupes(String s){
             char [] stack=new char[s.length()];
-            
+            int i=0;
+            for(int j=0;j<s.length();j++){
+                
+            }
         }
     }
     public static void main(String[] args){
