91a0ee62b379653309c86e20c7f20575b57a0420
Rahul <rahulkotr1@gmail.com>
2022-05-17 22:51:18 +0530

    count Characters

--- Java/Problem/countChars.java
+++ Java/Problem/countChars.java
@@ -1,8 +1,10 @@
 class countChars{
     class Solution{
-
+        public int countCharacter(String [] words, String chars){
+            
+        }
     }
     public static void main(String args[]){
-        
+
     }
 }
