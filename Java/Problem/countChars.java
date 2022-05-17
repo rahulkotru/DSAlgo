297ec80a76923fe2a6fedf65235cf15db83ae4d3
Rahul <rahulkotr1@gmail.com>
2022-05-17 22:53:04 +0530

    count Characters

--- Java/Problem/countChars.java
+++ Java/Problem/countChars.java
@@ -3,6 +3,9 @@ class countChars{
         public int countCharacter(String [] words, String chars){
             int goodWordLengthSum=0;
             int [] charCount=new int[26];
+            for (char c: chars.toCharArray()){
+                
+            }
         }
     }
     public static void main(String args[]){
