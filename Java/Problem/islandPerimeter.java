274b6625dff4cb3aad37466bea6bacd028b1e9cc
Rahul <rahulkotr1@gmail.com>
2022-05-18 20:59:13 +0530

    Island perimeter

--- Java/Problem/islandPerimeter.java
+++ Java/Problem/islandPerimeter.java
@@ -7,6 +7,7 @@ class islandPerimeter{
             int result=0;
             for(int i=0; i<grid.length;i++){
                 for(int j=0;j<grid.length;j++){
+                    result+=4;
                     
                 }
             }
