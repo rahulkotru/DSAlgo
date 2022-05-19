4a68acef93ef52275d202e7f6067f11579c32fc6
Rahul <rahulkotr1@gmail.com>
2022-05-19 23:04:01 +0530

    Contains String

--- Java/Problem/containsString.java
+++ Java/Problem/containsString.java
@@ -1,7 +1,7 @@
 class containsString{
     class Solution{
         public boolean rotateString(String A, String B){
-            return (A.length()==B.length() && (A+A))
+            return (A.length()==B.length() && (A+A).contains())
         }
     }
     public static void main(String args[]){
