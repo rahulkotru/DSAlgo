cb426dccb89cc1d54885bf35860141525821587d
Rahul <rahulkotr1@gmail.com>
2022-05-20 23:37:45 +0530

    max Islands

--- Java/Problem/maxIslands.java
+++ Java/Problem/maxIslands.java
@@ -2,6 +2,7 @@ class maxIslands{
     class Solution{
         public int findConsecutiveMax(int [] nums){
             int max=0;
+            int currentMax=0;
         }
     }
     public static void main(String[] args){
