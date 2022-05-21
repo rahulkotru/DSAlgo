c6381a57490d828c23f4b6457d4193f479ad6cd7
Rahul <rahulkotr1@gmail.com>
2022-05-21 23:36:27 +0530

    longest words

--- Java/Problem/longestWordDict.java
+++ Java/Problem/longestWordDict.java
@@ -3,6 +3,7 @@ class longestWordDict{
         public String longestWord(String[] words){
             Arrays.sort(words);
             HashSet<String> builtWords=new HashSet();
+            String results="";
         }
     }
     public static void main(String[] args){
