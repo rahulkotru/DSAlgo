8acc7cef8f6c90cae07e5e2ba6bad0abfa8cf0b1
Rahul <rahulkotr1@gmail.com>
2022-05-22 23:37:52 +0530

    FizzBuzz

--- Java/Problem/fizzBuzz.java
+++ Java/Problem/fizzBuzz.java
@@ -6,7 +6,15 @@ class fizzBuzz{
                 fizz++;
                 buzz++;
                 if(fizz==3&&buzz==5){
-                    
+
+                }
+                else if(fizz==3){
+                    fizz=0;
+                    buzz==0;
+                }
+                else if(fizz==3){
+                    fizz=0;
+                    outputArr.add("Fizz");
                 }
             }
         }
