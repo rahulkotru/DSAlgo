9ece5e1edda113965192a231c7abacb5822b4a33
Rahul <rahulkotr1@gmail.com>
2022-05-22 23:36:39 +0530

    FizzBuzz

--- Java/Problem/fizzBuzz.java
+++ Java/Problem/fizzBuzz.java
@@ -3,7 +3,11 @@ class fizzBuzz{
         public List<String> fizzB(int n){
             List<String> outputArr=new ArrayList();
             for(int i=1,fizz=0,buzz=0;i<=n;i++){
-                
+                fizz++;
+                buzz++;
+                if(fizz==3&&buzz==5){
+                    
+                }
             }
         }
 
