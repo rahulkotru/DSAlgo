a6ef40b6d3b9e203b880230892a4c40f17b6bea3
Rahul <rahulkotr1@gmail.com>
2022-05-22 23:34:37 +0530

    FizzBuzz

--- Java/Problem/fizzBuzz.java
+++ Java/Problem/fizzBuzz.java
@@ -1,7 +1,7 @@
 class fizzBuzz{
     class Solution{
         public List<String> fizz(int n){
-            
+            List<String> outputArr=new ArrayList();
         }
     }
     public static void main(String[] args) {
