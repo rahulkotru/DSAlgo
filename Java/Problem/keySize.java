d522f2a42ebdaaa63583bdce18215517657e75cd
Rahul <rahulkotr1@gmail.com>
2022-05-26 23:39:59 +0530

    Key size

--- Java/Problem/keySize.java
+++ Java/Problem/keySize.java
@@ -2,7 +2,7 @@ class keySize{
     class Solution {
         public String keyCheck(String key, int k){
             key=key.toUpperCase();
-            
+            key=key.replaceAll("-"," ");
         }
     }
     public static void main(String[] args){
