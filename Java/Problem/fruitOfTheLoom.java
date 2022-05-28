874daf8c2a8284be8767baf3cdb7357764429707
Rahul <rahulkotr1@gmail.com>
2022-05-28 22:07:04 +0530

    Fruit into basket

--- Java/Problem/fruitOfTheLoom.java
+++ Java/Problem/fruitOfTheLoom.java
@@ -1,8 +1,10 @@
 class fruitOfTheLoom{
     class Solution{
-
+        public int totalFruit(int[] tree){
+            
+        }
     }
     public static void main(String [] args){
-        
+
     }
 }
