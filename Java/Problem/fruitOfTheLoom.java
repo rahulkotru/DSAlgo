89e62be4362728330074ffe81a791032497894f4
Rahul <rahulkotr1@gmail.com>
2022-05-28 22:08:15 +0530

    Fruit into basket

--- Java/Problem/fruitOfTheLoom.java
+++ Java/Problem/fruitOfTheLoom.java
@@ -1,7 +1,9 @@
 class fruitOfTheLoom{
     class Solution{
         public int totalFruit(int[] tree){
-            
+            int lastFruit = -1;
+            int secondLast = -1;
+            int lastFruitCount=
         }
     }
     public static void main(String [] args){
