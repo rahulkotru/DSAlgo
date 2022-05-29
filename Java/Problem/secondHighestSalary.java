ff4217aa82c94518912b1f13cef9c88bea3b9685
Rahul <rahulkotr1@gmail.com>
2022-05-29 21:04:01 +0530

    City skyline

--- Java/Problem/secondHighestSalary.java
+++ Java/Problem/secondHighestSalary.java
@@ -2,6 +2,7 @@ class secondHighestSalary{
     class Solution {
         public int maxIncreaseSkyline(int grid[][]){
             int result = 0;
+            int n=grid.length;
 
             result+=min(maxCol,maxRow)-grid[i][j];
         }
