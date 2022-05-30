0a75cf6ff42f300688bb1e15e633b119b9f61a7a
Rahul <rahulkotr1@gmail.com>
2022-05-30 23:33:56 +0530

    Phone Number

--- Java/Problem/phoneNumber.java
+++ Java/Problem/phoneNumber.java
@@ -3,7 +3,8 @@
         public List<String> letter(String digits){
             LinkedList<String> outputArr= new LinkedList();
             if(digits.length()==0) return outputArr;
-        }
+            outputArr.add("");
+        }   
     } 
     public static void main(String[] args){
 
