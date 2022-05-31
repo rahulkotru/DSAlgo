ff11c927aebbcb35e63d1d0d86ee52aa0233ea60
Rahul <rahulkotr1@gmail.com>
2022-05-31 23:38:21 +0530

    Reorder linked list

--- Java/Problem/reorderLinkedList.java
+++ Java/Problem/reorderLinkedList.java
@@ -2,6 +2,9 @@ class reorderLinkedList{
     class ListNode{
         ListNode next;
         int value;
+        ListNode(int x){
+            this.val=x;
+        }
     }
     class Solution {
         public void reorderList(ListNode head){
