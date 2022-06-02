99169fb1c31a010b049d17d85366ba51aa75f8c1
Rahul <rahulkotr1@gmail.com>
2022-06-02 23:42:03 +0530

    Most Common words

--- Java/Problem/commonWords.java
+++ Java/Problem/commonWords.java
@@ -3,6 +3,9 @@ class commonWords{
         public String mostCommon(String paragraph,String[] banned){
             HashSet<String> bannedWords= new HashSet();
             HashMap<String,Integer> validWords=new HashMap();
+            for (String bannedWord: banned){
+                
+            }
         } 
     }
     public static void main(String[] args){
