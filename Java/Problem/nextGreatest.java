973d6d4b4a458cf0084103cc14cdb9d9a0d6586e
Rahul <rahulkotr1@gmail.com>
2022-06-03 23:50:35 +0530

    next greatest thing

--- Java/Problem/nextGreatest.java
+++ Java/Problem/nextGreatest.java
@@ -3,6 +3,9 @@ class nextGreatest{
         public int[] greatest(int [] nums1, int[] nums){
             HashMap<Integer,Integer> next= new HashMap();
             Stack<Integer> stack=new Stack();
+            for(Integer num: nums2){
+                
+            }
     }
     public static void main(String[] args){
 
