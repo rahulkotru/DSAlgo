77a0f846ea7f15f26ac74088e6c3946d12778fd4
Rahul <rahulkotr1@gmail.com>
2022-06-03 23:51:27 +0530

    next greatest thing

--- Java/Problem/nextGreatest.java
+++ Java/Problem/nextGreatest.java
@@ -4,7 +4,9 @@ class nextGreatest{
             HashMap<Integer,Integer> next= new HashMap();
             Stack<Integer> stack=new Stack();
             for(Integer num: nums2){
-                
+               if(!Stack.isEmpty()&& stack.peek()<num) {
+                   
+               }
             }
     }
     public static void main(String[] args){
