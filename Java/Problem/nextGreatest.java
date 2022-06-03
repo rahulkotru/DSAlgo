fc98623809507f402e44546f5bae15083cc97c76
Rahul <rahulkotr1@gmail.com>
2022-06-03 23:49:38 +0530

    next greatest thing

--- Java/Problem/nextGreatest.java
+++ Java/Problem/nextGreatest.java
@@ -1,7 +1,7 @@
 class nextGreatest{
     class Solution {
         public int[] greatest(int [] nums1, int[] nums){
-            
+            HashMap<Integer,Integer> next= new HashMap();
         }
     }
     public static void main(String[] args){
