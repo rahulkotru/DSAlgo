100a31fc280fa2a5872b6b40a07138ac0eeb0a10
Rahul <rahulkotr1@gmail.com>
2022-06-04 23:53:04 +0530

    Shortest distance to char

--- Java/Problem/shortestDistanceToChar.java
+++ Java/Problem/shortestDistanceToChar.java
@@ -4,6 +4,9 @@ class shortestDistanceToChar{
         public int[] distance(String S, char C){
             int n=S.length;
             int[] output = new int[n];
+            int cPos=-n;
+            
+            for(int)
         }
     }
     public static void main(String[] args){
