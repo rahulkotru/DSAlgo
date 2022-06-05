7bb0d4dae052ebbe013d7628fbcc920a74b7674e
Rahul <rahulkotr1@gmail.com>
2022-06-05 23:15:19 +0530

    Check long press

--- Java/Problem/longPressed.java
+++ Java/Problem/longPressed.java
@@ -2,7 +2,7 @@ class longPressed{
     class Solution {
         public boolean checkDoublePress(String name, String typed){
             if (name.length== typed.length) return true;
-            
+            if(name.length>typed.length) return false;
         }
     }
     public static void main(String[] args){
