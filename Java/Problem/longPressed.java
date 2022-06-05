c746eabdf2ed80c0070b6699e66e82321b87f573
Rahul <rahulkotr1@gmail.com>
2022-06-05 23:17:28 +0530

    Check long press

--- Java/Problem/longPressed.java
+++ Java/Problem/longPressed.java
@@ -8,7 +8,12 @@ class longPressed{
             int j=0;
 
             while(j<typed.length()){
-                
+                if(i<name.length()&& name.charAt(i)==typed.charAt(j)){
+                    i++;
+                }
+                else if(j==0||typed.charAt(j)!= typed.charAt(j-1)){
+                    
+                }
             }
         }
     }
