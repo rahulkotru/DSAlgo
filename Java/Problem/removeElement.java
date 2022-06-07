de9b8662649eb2ec2fdf8ea328a270c490d43e5b
Rahul <rahulkotr1@gmail.com>
2022-06-07 23:01:30 +0530

    remove element

--- Java/Problem/removeElement.java
+++ Java/Problem/removeElement.java
@@ -2,6 +2,8 @@ class removeElement{
     class Solution{
         public int remove(int [] nums, int val){
             if(nums.length==0) return 0;
+            int j=0;
+            
         }
     }
     public static void main(String[] args){
