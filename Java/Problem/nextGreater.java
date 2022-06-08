ca7abe981408df34af7f43942a353046220515aa
Rahul <rahulkotr1@gmail.com>
2022-06-08 22:06:13 +0530

    Next Greater

--- Java/Problem/nextGreater.java
+++ Java/Problem/nextGreater.java
@@ -1,10 +1,12 @@
 class nextGreater{
     class ListNode{
-
+        ListNode next;
+        int val;
     }
     class Solution {
-        }
-        public static void main(String[] args){
-            
-        }
+
+    }
+    public static void main(String[] args){
+
+    }
 }
