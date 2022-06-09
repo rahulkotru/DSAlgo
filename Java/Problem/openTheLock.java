9386468fb6b6c5e5b435c7bc5fcbe707f4efe55a
Rahul <rahulkotr1@gmail.com>
2022-06-09 23:26:21 +0530

    Open the locks

--- Java/Problem/openTheLock.java
+++ Java/Problem/openTheLock.java
@@ -2,6 +2,8 @@ class openTheLock{
     class Solution {
     public int openLock(String [] deadends, String target){
         HashSet<String> deadEnds= new HashSet(Arrays.asList(deadends));
+        HashSet<String> visited= new HashSet();
+        
     }
     }
     public static void main(String[] args){
