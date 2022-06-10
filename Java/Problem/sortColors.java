cab3ea4917adad535eaf61790c61648584a4c8c8
Rahul <rahulkotr1@gmail.com>
2022-06-10 23:05:05 +0530

    sort colors

--- Java/Problem/sortColors.java
+++ Java/Problem/sortColors.java
@@ -1,7 +1,7 @@
 class sortColors{
     class Solution{
         public void sort(int [] nums){
-            
+            if(nums.length==0|| nums.length==1) return;
         }
     }
     public static void main(String[] args){
