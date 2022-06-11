4aa567020ecee2dc01b247d8acd517721cf107f9
Rahul <rahulkotr1@gmail.com>
2022-06-11 23:41:33 +0530

    Most Water

--- C++/Problem/mostWater.cpp
+++ C++/Problem/mostWater.cpp
@@ -4,7 +4,9 @@ class Solution{
     int maxArea(vector<int>& height){
         int minHeight=height[0];
         int maxHeight=height[height.size()-1];
-        
+        if(maxHeight< minHeight){
+            
+        }
     }
 }
 int main(String args){
