568ec70a6ffd1cb6cf643a086ecf7441b0abbbd2
Rahul <rahulkotr1@gmail.com>
2022-06-13 23:44:46 +0530

    Lemonade change

--- Java/Problem/lemonadeChange.java
+++ Java/Problem/lemonadeChange.java
@@ -1,7 +1,11 @@
 class lemonadeChange{
     class Solution{
         public boolean lemonadeChange(int[] bills){
-            
+            int fives=0;
+            int tens=0;
+            for(Integer bill :bills){
+                
+            }
         }
     }
     
