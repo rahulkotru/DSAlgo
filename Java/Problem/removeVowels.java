55a830ceeb0c2be3ad3516779aabd080163cffb2
Rahul <rahulkotr1@gmail.com>
2022-06-14 23:18:17 +0530

    Replace vowels

--- Java/Problem/removeVowels.java
+++ Java/Problem/removeVowels.java
@@ -2,7 +2,7 @@ class removeVowels{
     class Solution{
         public String remove(String S){
             if (S.equals("")) return "";
-            return S.replaceAll("[aeiou]",)
+            return S.replaceAll("[aeiou]","");
         }
     }
     public static void main(String[] args){
