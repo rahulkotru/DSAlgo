498fe8126947070a6822da4a6d92f59d0229e897
Rahul <rahulkotr1@gmail.com>
2022-06-14 23:17:19 +0530

    Replace vowels

--- Java/Problem/removeVowels.java
+++ Java/Problem/removeVowels.java
@@ -1,7 +1,7 @@
 class removeVowels{
     class Solution{
         public String remove(String S){
-            
+            if (S.equals("")) return "";
         }
     }
     public static void main(String[] args){
