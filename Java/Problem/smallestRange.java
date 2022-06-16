29105a79840d434977af741d329cd62d098c7d37
Rahul <rahulkotr1@gmail.com>
2022-06-16 23:34:50 +0530

    smallest range

--- Java/Problem/smallestRange.java
+++ Java/Problem/smallestRange.java
@@ -1,8 +1,10 @@
 class smallestRange{
     class Solution {
-
+        public int smallest(int [] A, int K){
+            
+        }
     }
     public static void main(String[] args){
-        
+
     }
 }
