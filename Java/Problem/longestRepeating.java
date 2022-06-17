83699d656eca61d5d235997184c4805f9728fdfb
Rahul <rahulkotr1@gmail.com>
2022-06-17 22:59:05 +0530

    LongestRepeating

--- Java/Problem/longestRepeating.java
+++ Java/Problem/longestRepeating.java
@@ -2,9 +2,10 @@ class longestRepeating{
     class Solution {
         public int longestInString(String s, int k){
             int N= s.length();
+            int charCount=new int[26];
             int windowStart= 0;
             for (int windowEnd=0;windowEnd<N;windowEnd++){
-                
+
             }
         }
     }
