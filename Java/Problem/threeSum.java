a800434b3ea54190be6bca20d2aff72a4b9f539d
Rahul <rahulkotr1@gmail.com>
2022-06-18 23:02:49 +0530

    Three Sum

--- Java/Problem/threeSum.java
+++ Java/Problem/threeSum.java
@@ -4,7 +4,9 @@ class threeSum{
         public List<List<Integer>> three(int [] nums){
             Arrays.sort(nums);
             List<List<Integer>> output= new LinkedList();
-            
+            for(int i=0; i<nums.length-2;i++){
+                
+            }
         }
     }
     public static void main(String[] args){
