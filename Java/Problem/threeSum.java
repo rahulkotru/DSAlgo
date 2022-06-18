9a0071ddbfe8ca4571d8be7685f931406ac187ea
Rahul <rahulkotr1@gmail.com>
2022-06-18 23:01:25 +0530

    Three Sum

--- Java/Problem/threeSum.java
+++ Java/Problem/threeSum.java
@@ -2,7 +2,7 @@ class threeSum{
 
     class Solution {
         public List<List<Integer>> three(int [] nums){
-            
+            Arrays.sort(nums);
         }
     }
     public static void main(String[] args){
