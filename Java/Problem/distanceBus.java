abcb92054f6240e555d091403c5fb98017a1b234
Rahul <rahulkotr1@gmail.com>
2022-06-19 23:46:54 +0530

    Distance between buses

--- Java/Problem/distanceBus.java
+++ Java/Problem/distanceBus.java
@@ -3,6 +3,7 @@ class distanceBus{
     class Solution {
         public int distanceBusStop(int [] distance, int start, int destination){
             int clockwiseDistance=0;
+            int totalDistance=0;
         }
         }
         public static void main(String[] args){
