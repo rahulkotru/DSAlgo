7068c425906795822e2847e8799d5c178df1deeb
Rahul <rahulkotr1@gmail.com>
2022-06-20 22:58:50 +0530

    Search rotated array

--- Java/Problem/searchRotatedArray.java
+++ Java/Problem/searchRotatedArray.java
@@ -4,7 +4,9 @@ class searchRotatedArray{
             if(nums==null || nums.length==0) return 1;
             int left =0;
             int right=nums.length-1;
-            
+            while (left< right){
+                int
+            }
         }
     }
     public static void main(String[] args){
