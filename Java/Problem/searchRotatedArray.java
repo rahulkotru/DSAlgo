81f6fbd6d1a38c64d2df6dfb6a3c7a882932ece7
Rahul <rahulkotr1@gmail.com>
2022-06-20 22:54:18 +0530

    Search rotated array

--- Java/Problem/searchRotatedArray.java
+++ Java/Problem/searchRotatedArray.java
@@ -1,8 +1,10 @@
 class searchRotatedArray{
     class Solution {
-    
+        public int search(int [] nums, int target){
+            
+        }
     }
     public static void main(String[] args){
-        
+
     }
 }
