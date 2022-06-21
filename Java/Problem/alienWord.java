cf71f57180903c2f837a3d43d93cc7533ffceed0
Rahul <rahulkotr1@gmail.com>
2022-06-21 23:29:00 +0530

    Alien Language

--- Java/Problem/alienWord.java
+++ Java/Problem/alienWord.java
@@ -1,7 +1,9 @@
 class alienWorld{
     class Solution {
         public int[] charMap;
-        public boolean alienSort(String [] words)
+        public boolean alienSort(String [] words, String order){
+            
+        }
     }
     public static void main(String args[]){
 
