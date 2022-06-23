16812d25fd28b3706cd7e347164209913071a197
Rahul <rahulkotr1@gmail.com>
2022-06-23 23:06:34 +0530

    Longest Palindrome

--- Java/Problem/longestPalindrome.java
+++ Java/Problem/longestPalindrome.java
@@ -2,6 +2,9 @@ class longestPalindrome{
     class Solution {
         public int longestP(String s){
             int[] charCount= new int[128];
+            for(char c:String.toCharArray()){
+                
+            }
         }
     }
     public static void main(String[] args){
