f07568d49e664d610c77788e267f46e110102ca3
Rahul <rahulkotr1@gmail.com>
2022-06-25 23:31:19 +0530

    Dupes in Array

--- Java/Problem/dupeArray.java
+++ Java/Problem/dupeArray.java
@@ -3,7 +3,7 @@ class dupeArray{
         public static List<Integer> findDupes(int[] nums ){
             List<Integer> outputArray= new ArrayList();
             for( int i=0;i<nums.length;i++){
-                
+                int index = Math.abs(nums[i]-1);
             }
         }
     }
