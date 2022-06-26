23027962fffde6ff72def72d2daa1d9f31f44352
Rahul <rahulkotr1@gmail.com>
2022-06-26 23:47:30 +0530

    Parenthesis generator

--- Java/Problem/parenthesisGenerator.java
+++ Java/Problem/parenthesisGenerator.java
@@ -1,7 +1,7 @@
 class parenthesisGenerator{
     class Solution{
         public List<String> generate(int n){
-            
+            List<String> outputArr= new ArrayList();
         }
     }
     public static void main(String[] args){
