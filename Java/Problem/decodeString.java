b77f334fa47ac6bb301bc202781a06effd0fd69a
Rahul <rahulkotr1@gmail.com>
2022-07-01 23:42:58 +0530

    Decode encoded string

--- Java/Problem/decodeString.java
+++ Java/Problem/decodeString.java
@@ -1,7 +1,7 @@
 class decodeString{
     class Solution{
         public String encoded(String s){
-            
+            Stack<Integer> counts= new Stack();
         }
     }
     public static void main(String[] args){
