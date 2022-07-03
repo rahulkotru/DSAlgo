abb1776bcf158cb9ccdcd6dbf15f214f4fac63ab
Rahul <rahulkotr1@gmail.com>
2022-07-03 23:38:05 +0530

    buddy strings

--- Java/Problem/buddyStrings.java
+++ Java/Problem/buddyStrings.java
@@ -2,7 +2,8 @@ class buddyStrings{
     class Solution {
         public boolean buddyString(String A, String B){
             if(A.length()!=B.length()) return false;
-            
+
+            List<Intger> diff=new ArrayList();
         }
     }
     public static void main(String[] args){
