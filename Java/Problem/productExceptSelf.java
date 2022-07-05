9cfdeb5a2cf256a82f117aa5e20ee0df3f5cb7ab
Rahul <rahulkotr1@gmail.com>
2022-07-05 23:38:34 +0530

    Except self

--- Java/Problem/productExceptSelf.java
+++ Java/Problem/productExceptSelf.java
@@ -1,8 +1,10 @@
 class productExceptSelf{
     class Solution{
-
+        public int[] product(int [] arr){
+            
+        }
     }
     public static void main(String[] args){
-        
+
     }
 }
