f9f65313cbe3fa4b78b9c25e9e3dccf9cbf2e40c
Rahul <rahulkotr1@gmail.com>
2022-07-05 23:43:32 +0530

    Except self

--- Java/Problem/productExceptSelf.java
+++ Java/Problem/productExceptSelf.java
@@ -3,7 +3,7 @@ class productExceptSelf{
         public int[] product(int [] nums){
             int N= nums.length;
             int [] leftProds=new int[N];
-            
+            int [] rightProds=new int[N];
         }
     }
     public static void main(String[] args){
