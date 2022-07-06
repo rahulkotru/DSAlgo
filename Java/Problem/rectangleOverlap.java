49e103d608b4d8a4630f456f6a343c9c1adcdffc
Rahul <rahulkotr1@gmail.com>
2022-07-06 23:28:21 +0530

    Area of intersection

--- Java/Problem/rectangleOverlap.java
+++ Java/Problem/rectangleOverlap.java
@@ -1,7 +1,7 @@
 class rectangleOverlap{
     class Solution{
         public boolean isRect(int rec1[], int rec2[]){
-            return rect1[0]<rect2[2] && rect1[1] <rec2[3] && rec2[0]<rec1[2]&&rec2[1]
+            return rect1[0]<rect2[2] && rect1[1] <rec2[3] && rec2[0]<rec1[2]&&rec2[1]< rec2[1];
         }
     }
     public static void main(String[] args){
