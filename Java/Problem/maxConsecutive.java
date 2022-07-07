81254d48b50aaf157b72a55a23bf1f2eb718fb09
Rahul <rahulkotr1@gmail.com>
2022-07-07 23:45:58 +0530

    Max contagious

--- Java/Problem/maxConsecutive.java
+++ Java/Problem/maxConsecutive.java
@@ -1,6 +1,8 @@
 class maxConsecutive{
     class Solution{
-        public int longestOnes(int nums[])
+        public int longestOnes(int nums[], int target){
+            
+        }
     }
     public static void main(String[] args){
 
