07f134c5705b87b775f9eb7f04a1171b1e90119f
Rahul <rahulkotr1@gmail.com>
2022-07-09 23:57:04 +0530

    length of longest substring

--- Java/Problem/lengthOfLongestSubstring.java
+++ Java/Problem/lengthOfLongestSubstring.java
@@ -1,7 +1,7 @@
 class lengthOfLengthSubstring{
     class Solution{
         public int length(String S){
-            
+           int i=0; 
         }
     }
     public static void main (String[] args){
