bee60863ac46b42dd903831d7038ee1f80a07862
Rahul <rahulkotr1@gmail.com>
2022-07-10 23:01:04 +0530

    Subarray sum

--- Java/Problem/subarraySum.java
+++ Java/Problem/subarraySum.java
@@ -1,7 +1,7 @@
 class subarraySum{
     class Solution{
         public int maxSubarray(int [] target){
-            
+            int sum=0;
         }
     }
     public static void main(String[] args){
