f2fd7c6e98e5cf7ecdba0857fa3ba912ae310662
Rahul <rahulkotr1@gmail.com>
2022-07-11 23:30:02 +0530

    Min climb stair

--- Java/Problem/minCostClimb.java
+++ Java/Problem/minCostClimb.java
@@ -2,7 +2,7 @@ class minCostClimb{
     class Solution {
         public int minCost(int [] cost){
             for(int i=2;i<cost.length;i++){
-                
+                cost[i]=Math.min(cost[i-1],cost[i-2]);
             }
         }
     }
