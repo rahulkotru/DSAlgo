3d492cc9300fd634ff5b1a2482e6314273e87feb
Rahul <rahulkotr1@gmail.com>
2022-07-22 23:32:20 +0530

    Position of element

--- Java/Problem/positionOfElement.java
+++ Java/Problem/positionOfElement.java
@@ -1,6 +1,8 @@
 class positionOfElement{
     class Solution{
-        public int [] searchRange(int[] nums,int target)
+        public int [] searchRange(int[] nums,int target){
+            
+        }
     }
     public static void main(String[] args){
 
