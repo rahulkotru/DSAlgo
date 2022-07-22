d3dcffe06f45a2fbe8043ef94dd8f505ff9aa360
Rahul <rahulkotr1@gmail.com>
2022-07-22 23:32:55 +0530

    Position of element

--- Java/Problem/positionOfElement.java
+++ Java/Problem/positionOfElement.java
@@ -1,7 +1,12 @@
 class positionOfElement{
+    class ListNode{
+        int data;
+        ListNode head;
+        List
+    }
     class Solution{
         public int [] searchRange(int[] nums,int target){
-            
+
         }
     }
     public static void main(String[] args){
