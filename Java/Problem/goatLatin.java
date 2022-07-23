a6b4227565a05b559e0e72c7b948ea81e01c1195
Rahul <rahulkotr1@gmail.com>
2022-07-23 23:09:48 +0530

    Goat Latin

--- Java/Problem/goatLatin.java
+++ Java/Problem/goatLatin.java
@@ -2,7 +2,12 @@ class goatLatin{
     class Solution{
         public String toGoat(String S){
             HashSet<Character> vowels=new HashSet();
+            for(c:"aeiouAEIOU".toCharArray()){
+                
+            }
             String result="";
+
+
         }
     }
     public static void main(String[] args){
