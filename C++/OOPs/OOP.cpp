dab35f2487563db9abc71d5e7d29c08bb511ac3e
Rahul Kotru <46603416+rahulkotru@users.noreply.github.com>
2022-09-02 22:29:32 +0530

    Test

--- C++/OOPs/OOP.cpp
+++ C++/OOPs/OOP.cpp
@@ -95,7 +95,7 @@ class Teacher :public Employee{
     void work(){
         cout<<Name<<" is giving lecture on "<<subject<<"."<<endl;//Polymorphism
     }
-    };123
+    };1234
 int main(){
     Employee Phil=Employee("Phil","Tata",24);
     Employee E2;
